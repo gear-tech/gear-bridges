92b0ce7bdf959e0164c28114a0a41f64d57da552
LouiseMedova <luisa.medova@gmail.com>
2024-01-30 10:54:28 +0300

    changes eth-client

--- eth-client/src/lib.rs
+++ eth-client/src/lib.rs
@@ -51,7 +51,7 @@ impl ContractVerifiers {
             publics[18],
         ];
 
-        let nonce_id = publics[13];
+        let validator_set_id = publics[13];
 
         let account = {
             let key: PrivateKey = pk.parse().expect("invalid PK");
@@ -59,19 +59,19 @@ impl ContractVerifiers {
         };
 
         self.vs_change_vrf
-            .verify_validator_set_change_proof(p_a, p_b, p_c, validator_set, nonce_id)
+            .verify_validator_set_change_proof(p_a, p_b, p_c, validator_set, validator_set_id)
             .from(account)
             .send()
             .await
             .map_err(|_| VerifierError::ErrorDuringContractExecution)?;
 
-        let exp_nonce_id = self
+        let validator_set_id = self
             .vs_change_vrf
-            .get_nonce_id()
+            .get_validator_set_id()
             .call()
             .await
             .map_err(|_| VerifierError::ErrorDuringContractExecution)?;
-        if exp_nonce_id != nonce_id {
+        if validator_set_id != validator_set_id {
             return Ok(false);
         }
         Ok(true)
@@ -91,7 +91,7 @@ impl ContractVerifiers {
             Account::Offline(key, None)
         };
 
-        let validator_set = [
+        let msg_hashes = [
             publics[14],
             publics[15],
             publics[16],
@@ -99,22 +99,22 @@ impl ContractVerifiers {
             publics[18],
         ];
 
-        let nonce_id = publics[13];
+        let validator_set_id = publics[13];
 
         self.msg_sent_vrf
-            .verify_msg_sent_proof(p_a, p_b, p_c, validator_set, nonce_id)
+            .verify_msg_sent_proof(p_a, p_b, p_c, msg_hashes, validator_set_id)
             .from(account)
             .send()
             .await
             .map_err(|_| VerifierError::ErrorDuringContractExecution)?;
 
-        let exp_nonce_id = self
+        let exp_msg_hashes = self
             .msg_sent_vrf
-            .get_nonce_id()
+            .get_last_msg_hashes()
             .call()
             .await
             .map_err(|_| VerifierError::ErrorDuringContractExecution)?;
-        if exp_nonce_id != nonce_id {
+        if exp_msg_hashes != msg_hashes {
             return Ok(false);
         }
         Ok(true)
@@ -147,41 +147,30 @@ impl ContractVerifiers {
     ) -> Result<U256, VerifierError> {
 
         Ok(self.vs_change_vrf
-            .get_nonce_id()
+            .get_validator_set_id()
             .call()
             .await
             .map_err(|_| VerifierError::ErrorDuringContractExecution)?)
     }
 
 
-    pub async fn get_all_validator_sets_from_msg_sent_vrf(
+    pub async fn get_all_msg_hashes_from_msg_sent_vrf(
         &self,
     ) -> Result<Vec<[U256; 5]>, VerifierError> {
 
         Ok(self.msg_sent_vrf
-            .get_all_validator_sets()
+            .get_all_msg_hashes()
             .call()
             .await
             .map_err(|_| VerifierError::ErrorDuringContractExecution)?)
     }
 
-    pub async fn get_last_validator_set_from_msg_sent_vrf(
+    pub async fn get_last_msg_hashes_from_msg_sent_vrf(
         &self,
     ) -> Result<[U256; 5], VerifierError> {
 
         Ok(self.msg_sent_vrf
-            .get_last_validator_set()
-            .call()
-            .await
-            .map_err(|_| VerifierError::ErrorDuringContractExecution)?)
-    }
-
-    pub async fn get_nonce_id_from_msg_sent_vrf(
-        &self,
-    ) -> Result<U256, VerifierError> {
-
-        Ok(self.msg_sent_vrf
-            .get_nonce_id()
+            .get_last_msg_hashes()
             .call()
             .await
             .map_err(|_| VerifierError::ErrorDuringContractExecution)?)
@@ -298,44 +287,39 @@ mod tests {
         );
     }
 
-    #[tokio::test]
-    async fn msg_sent_verifier() {
-        let vs_change_vrf_address = "5FbDB2315678afecb367f032d93F642f64180aa3";
-        let msg_sent_vrf_address = "e7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-        let url = "http://127.0.0.1:8545/";
-        let verifier = ContractVerifiers::new(url, vs_change_vrf_address, msg_sent_vrf_address)
-            .expect("Error during verifiers instantiation");
+    // uncomment when there will be public inputs for msg sent verifier
 
-        println!(
-            "{:?}",
-            verifier
-                .verify_msg_sent(
-                    String::from("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"),
-                    "../solidity_verifier/aggregation/message_sent/final_proof.json",
-                    "../solidity_verifier/aggregation/message_sent/final_public.json"
-                )
-                .await
-        );
+    // #[tokio::test]
+    // async fn msg_sent_verifier() {
+    //     let vs_change_vrf_address = "5FbDB2315678afecb367f032d93F642f64180aa3";
+    //     let msg_sent_vrf_address = "e7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+    //     let url = "http://127.0.0.1:8545/";
+    //     let verifier = ContractVerifiers::new(url, vs_change_vrf_address, msg_sent_vrf_address)
+    //         .expect("Error during verifiers instantiation");
 
-        println!(
-            "{:?}",
-            verifier
-                .get_all_validator_sets_from_msg_sent_vrf()
-                .await
-        );
+    //     println!(
+    //         "{:?}",
+    //         verifier
+    //             .verify_msg_sent(
+    //                 String::from("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"),
+    //                 "../solidity_verifier/aggregation/message_sent/final_proof.json",
+    //                 "../solidity_verifier/aggregation/message_sent/final_public.json"
+    //             )
+    //             .await
+    //     );
 
-        println!(
-            "{:?}",
-            verifier
-                .get_last_validator_set_from_msg_sent_vrf()
-                .await
-        );
+    //     println!(
+    //         "{:?}",
+    //         verifier
+    //             .get_all_msg_hashes_from_msg_sent_vrf()
+    //             .await
+    //     );
 
-        println!(
-            "{:?}",
-            verifier
-                .get_nonce_id_from_msg_sent_vrf()
-                .await
-        );
-    }
+    //     println!(
+    //         "{:?}",
+    //         verifier
+    //             .get_last_msg_hashes_from_msg_sent_vrf()
+    //             .await
+    //     );
+    // }
 }

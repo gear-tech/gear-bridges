04021ae8fc978154bb15d3b6fa8035e62fdbf36b
mertwole <mertwole@gmail.com>
2024-03-18 14:53:07 +0000

    Implement scale compact integer parser

--- prover/src/storage_proof/node_parser/child_node_array_parser/scale_compact_integer_parser.rs
+++ prover/src/storage_proof/node_parser/child_node_array_parser/scale_compact_integer_parser.rs
@@ -1,26 +1,9 @@
-use std::iter;
+use plonky2::plonk::circuit_builder::CircuitBuilder;
+use plonky2_field::types::Field;
 
-use plonky2::{
-    iop::{
-        target::{BoolTarget, Target},
-        witness::{PartialWitness, WitnessWrite},
-    },
-    plonk::{circuit_builder::CircuitBuilder, circuit_data::CircuitConfig},
-};
-use plonky2_blake2b256::circuit::blake2_circuit_from_targets;
-use plonky2_field::{packed::PackedField, types::Field};
-
-use super::{NodeDataBlockTarget, PartialStorageAddressTarget};
 use crate::{
-    common::{
-        array_to_bits,
-        targets::{
-            impl_target_set, Blake2Target, ByteTarget, HalfByteTarget, SingleTarget, TargetSet,
-        },
-        xor_targets,
-    },
+    common::targets::{impl_target_set, ByteTarget, SingleTarget, TargetSet},
     prelude::*,
-    ProofWithCircuitData,
 };
 
 impl_target_set! {
@@ -40,5 +23,54 @@ pub fn define(
     input: ScaleCompactIntegerParserInputTarget,
     builder: &mut CircuitBuilder<F, D>,
 ) -> ScaleCompactIntegerParserOutputTarget {
-    todo!()
+    let bits = input.first_byte.to_bit_targets(builder);
+
+    builder.assert_zero(bits.0[0].target);
+    builder.assert_zero(bits.0[1].target);
+
+    let shift = builder.constant(F::from_canonical_u8(4));
+    let decoded = builder.div(input.first_byte.to_target(), shift).into();
+
+    ScaleCompactIntegerParserOutputTarget { decoded }
+}
+
+#[cfg(test)]
+mod tests {
+    use plonky2::{iop::witness::PartialWitness, plonk::circuit_data::CircuitConfig};
+
+    use super::*;
+
+    #[test]
+    fn test_scale_compact_integer_parser() {
+        test_case(0b00_00_00_00, Some(0));
+        test_case(0b11_11_11_00, Some(63));
+        test_case(0b00_00_11_00, Some(3));
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "Partition containing Wire(Wire { row: 0, column: 2 }) was set twice with different values: 0 != 1"
+    )]
+    fn test_scale_compact_integer_parser_fails_on_unsupported_prefix() {
+        test_case(0b00_00_00_10, None);
+    }
+
+    fn test_case(first_byte: u8, expected: Option<usize>) {
+        let config = CircuitConfig::standard_recursion_config();
+        let mut builder = CircuitBuilder::<F, D>::new(config);
+        let pw = PartialWitness::new();
+
+        let first_byte = ByteTarget::constant(first_byte, &mut builder);
+        let input = ScaleCompactIntegerParserInputTarget { first_byte };
+        let output = define(input, &mut builder);
+
+        if let Some(expected) = expected {
+            let expected = builder.constant(F::from_canonical_usize(expected));
+            builder.connect(output.decoded.to_target(), expected);
+        }
+
+        let circuit = builder.build::<C>();
+        let proof = circuit.prove(pw).expect("Failed to prove");
+        circuit.verify(proof).expect("Failed to verify");
+    }
 }

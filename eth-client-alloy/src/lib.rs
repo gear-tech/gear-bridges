a87e9f67c8b20458d73bc270d9337bac81858910
Eugene <koinoff@gmail.com>
2024-03-25 01:46:20 +0100

    test

--- eth-client-alloy/src/lib.rs
+++ eth-client-alloy/src/lib.rs
@@ -12,8 +12,8 @@ use alloy_transport::{BoxTransport, Transport};
 use alloy_rpc_client::RpcClient;
 use reqwest::{Client, Url};
 use alloy_primitives::{Address, B256, Bytes, hex, U256};
-use alloy_provider::layers::{ManagedNonceLayer, ManagedNonceProvider, SignerProvider};
-use alloy_signer_wallet::LocalWallet;
+use alloy_provider::layers::{GasEstimatorLayer, GasEstimatorProvider, ManagedNonceLayer, ManagedNonceProvider, SignerProvider};
+use alloy_signer_wallet::{LocalWallet, Wallet};
 use alloy_sol_types::{sol, SolCall, SolInterface};
 use crate::abi::IRelayer::IRelayerInstance;
 use crate::abi::IMessageQueue::IMessageQueueInstance;
@@ -22,13 +22,15 @@ use crate::convert::Convert;
 use crate::proof::Proof;
 use std::marker::PhantomData;
 use alloy_rpc_types::TransactionRequest;
+use alloy_signer::k256::ecdsa::SigningKey;
 
 
-type ProviderType = ManagedNonceProvider<Ethereum, BoxTransport, SignerProvider<Ethereum, BoxTransport, RootProvider<Ethereum, BoxTransport>, EthereumSigner>>;
+type ProviderType = ManagedNonceProvider<Ethereum, BoxTransport, GasEstimatorProvider<Ethereum, BoxTransport, SignerProvider<Ethereum, BoxTransport, RootProvider<Ethereum, BoxTransport>, EthereumSigner>>>;
 
 
     pub struct ContractVerifiers
     {
+        signer : Wallet<SigningKey>,
         provider : Arc<ProviderType>,
         message_queue_instance : IMessageQueueInstance<Ethereum, BoxTransport , Arc<ProviderType>>,
         relayer_instance : IRelayerInstance<Ethereum, BoxTransport , Arc<ProviderType>>,
@@ -60,7 +62,8 @@ type ProviderType = ManagedNonceProvider<Ethereum, BoxTransport, SignerProvider<
 
             let provider  = Arc::new(ProviderBuilder::new()
                 .layer(ManagedNonceLayer)
-                .signer(EthereumSigner::from(signer)) // note the order!
+                .layer(GasEstimatorLayer)
+                .signer(EthereumSigner::from(signer.clone())) // note the order!
                 .provider(rp));
 
             let relayer_contract_address : Address = relayer_address_str.parse().map_err(|_| VerifierError::WrongAddress)?;
@@ -71,6 +74,7 @@ type ProviderType = ManagedNonceProvider<Ethereum, BoxTransport, SignerProvider<
             let message_queue_instance = IMessageQueue::new(message_queue_contract_address, provider.clone());
 
             Ok(ContractVerifiers{
+                signer,
                 provider,
                 relayer_instance,
                 message_queue_instance,
@@ -87,7 +91,8 @@ type ProviderType = ManagedNonceProvider<Ethereum, BoxTransport, SignerProvider<
             match self.relayer_instance.submit_merkle_root(public_inputs.clone(), proof.clone())
                 .estimate_gas().await {
                 Ok(gas_used) => {
-                    match self.relayer_instance.submit_merkle_root(public_inputs, proof).send().await {
+                    println!("Gas used: {gas_used}");
+                    match self.relayer_instance.submit_merkle_root(public_inputs, proof).from(self.signer.address()).gas_price(U256::from(2_000_000_000_000u128)).send().await {
                         Ok(pending_tx)=>{
                             match pending_tx.get_receipt().await {
                                 Ok(receipt)=>{
@@ -99,6 +104,7 @@ type ProviderType = ManagedNonceProvider<Ethereum, BoxTransport, SignerProvider<
                             }
                         }
                         Err(e)=>{
+                            println!("Sending error: {e:?}");
                             Err(VerifierError::ErrorSendingTransaction)
                         }
                     }
@@ -134,72 +140,43 @@ type ProviderType = ManagedNonceProvider<Ethereum, BoxTransport, SignerProvider<
         use crate::*;
 
         #[tokio::test]
-        async fn validator_set_change_verifier() {
-            let vs_change_vrf_address = "5FbDB2315678afecb367f032d93F642f64180aa3";
-            let msg_sent_vrf_address = "e7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+        async fn contract_verifier_create() {
+            let message_queue = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+            let replayer = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
+            let pk = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
             let url = "http://127.0.0.1:8545/";
-            let client = ContractVerifiers::new(url, vs_change_vrf_address, msg_sent_vrf_address, None).unwrap();
-            //let verifier = ContractVerifiers::new(url, vs_change_vrf_address, msg_sent_vrf_address)
-            //    .expect("Error during verifiers instantiation");
-
-            /*println!(
-                "{:?}",
-                verifier
-                    .verify_vs_change(
-                        String::from(
-                            "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-                        ),
-                        "../solidity_verifier/aggregation/vs_change/final_proof.json",
-                        "../solidity_verifier/aggregation/vs_change/final_public.json"
-                    )
-                    .await
-            );
-
-            println!("{:?}", verifier.get_all_validator_sets_from_vs_vrf().await);
-
-            println!("{:?}", verifier.get_last_validator_set_from_vs_vrf().await);
-
-            println!("{:?}", verifier.get_nonce_id_from_vs_vrf().await);
-
-            panic!();
-             */
+            let client = ContractVerifiers::new(url, message_queue, replayer, Some(pk)).unwrap();
         }
 
-        // uncomment when there will be public inputs for msg sent verifier
+        #[tokio::test]
+        async fn verify_block() {
+            let message_queue = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
+            let replayer = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707";
+            let pk = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+            let url = "http://127.0.0.1:8545/";
+            let client = ContractVerifiers::new(url, message_queue, replayer, Some(pk)).unwrap();
 
-        // #[tokio::test]
-        // async fn msg_sent_verifier() {
-        //     let vs_change_vrf_address = "5FbDB2315678afecb367f032d93F642f64180aa3";
-        //     let msg_sent_vrf_address = "e7f1725E7734CE288F8367e1Bb143E90bb3F0512";
-        //     let url = "http://127.0.0.1:8545/";
-        //     let verifier = ContractVerifiers::new(url, vs_change_vrf_address, msg_sent_vrf_address)
-        //         .expect("Error during verifiers instantiation");
+            let proof_json = r#"{
+                "proof" : "18d39978105e6371129a8c670c4958719bf0b860646c2dd760a14c6b5aa04b8e1682aec235c07cc291c2bc14670ab30db45b6c6ce53e7d6e42d5d4837a6a0120183d34eb74c7afdf6d88b54e1bde6948e7f566f6cc374e8bec0ab5553e2b95392ecb009497004b9defb864e8756bbfc830dc0e1f505687c9c4779a32f6783943262140c77797264ea54462073603c736a6c78b20a3016f5493f5cf95556ee81e29ed533dc33499c78e45b8c3c36993a6ad812b7073d8f4ca1a61da68b44e28d00cca5e1481a5bf5fea36beae27af01d45bf45ae9d239fd0e03943c7572c4a7bc2a6770a5201926e0d1c6779e580553bc7cfffafd226b0db88be65e8e9f8a77f90ead631a96254c7ad8b6138976435cb6685e7dd5f567290ac6a4e6e4715cdd441418e1ec0c96cca970d2edc68c95b14e42a0bedb073038588c452fcc3ab85c5d1725a1a7880200a962e465e0f9d3f17fc3159f80fbfd30dc098cdc1a99737c44091712fdc9915499cb86525dca25f08198a7b402679d863eb2a02445fad7e28429afaf7c029fe6de81b785f1453e2f44c0c97c0618519c25c955c64156bc4ebe108f6d877fd532555f808b338826e1234c20bb2ccb22da3115fc75d93e41b0b21bd41532aafe2c5ac3ce6cc421cd2c4617aefb685fe0edeaa4938e6dd517820d09da9f3f01d8ede516dac6789e50a13567d2e439eeafdbfa2591a3ddfb128853087aae48a9d53e1d8fb48ee4515b37291704f31cf4d884035920a722325c47d404f63a5ab3833cc17c7117d088197ede501a1d2aa5e26cfbc4946734edf825a80c0bd829d71a6ff5be13ff2c21cb0e3dce66f73f7c30deae6c08738a0b6f231502620c55b44eeb77d256650ba7ade32188a7b72a1758cfc9b0df08e96db5728d2da080f494511bb845c10e66678a76337ebb3dd38980c827543059a159f7fdb62383d97cb2a8b89e16bbefd2111f7d67f0f396e10468e916e85c56b65222294520172052b927228118ade9c2a5345d38831c1ec55bb06534ee94ba43c072f7fa2303ac1d8973c436bb1c7b32bb904bb14c0bf00d8aaf28ff1c7f1f4cf8f7767e105c59c10c4daf99ddc0bcfb3cf4d124613dc9beeee7432d69312f3173edf7d31b1920e827a8ac303e56138695f31ea541b623e6b42cf3fc32635b806dc2f80c1a9c32580fe608a068ce6ad82d81aec14d4ff6e4289716e2d775764554fa24cb2e6766d5885115b9ba39aabcfe166368906efca5c804adecb21f7e84a9ba51b91cac472170ed426ab2407c18a25e5dd9dbdefaceed5249559e537100d9aad4df",
+                "public_inputs" : [
+                    3544317610574872,
+                    3818006324670434,
+                    1609100126983798,
+                    2043470627881931,
+                    194624568354568,
+                    18446744069414584595
+                ]
+            }"#;
 
-        //     println!(
-        //         "{:?}",
-        //         verifier
-        //             .verify_msg_sent(
-        //                 String::from("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"),
-        //                 "../solidity_verifier/aggregation/message_sent/final_proof.json",
-        //                 "../solidity_verifier/aggregation/message_sent/final_public.json"
-        //             )
-        //             .await
-        //     );
+            match client.verify_merkle_root_from_json_string(proof_json).await {
+                Ok(result)=>{
+                    println!("Successfully verified : {result}")
+                }
+                Err(e)=>{
+                    println!("Error verifying : {e:?}")
+                }
+            }
 
-        //     println!(
-        //         "{:?}",
-        //         verifier
-        //             .get_all_msg_hashes_from_msg_sent_vrf()
-        //             .await
-        //     );
-
-        //     println!(
-        //         "{:?}",
-        //         verifier
-        //             .get_last_msg_hashes_from_msg_sent_vrf()
-        //             .await
-        //     );
-        // }
-
+        }
 
     }

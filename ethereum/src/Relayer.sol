233e26d503104a0c83184a922dd6d372af349cc0
Eugene Koinov <31383904+koinov@users.noreply.github.com>
2024-06-01 08:32:30 +0200

    Ethereum smart contracts (#52)

---
+++ ethereum/src/Relayer.sol
@@ -0,0 +1,103 @@
+pragma solidity ^0.8.24;
+
+import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
+import {IVerifier} from "./interfaces/IVerifier.sol";
+import {IRelayer} from "./interfaces/IRelayer.sol";
+import  "./libraries/Environment.sol";
+
+contract Relayer is IRelayer {
+    IVerifier private _verifier;
+    mapping(uint256 => bytes32) private _block_numbers;
+    mapping(bytes32 => uint256) private _merkle_roots;
+
+    uint256 private constant MASK_32BITS = (2 ** 32) - 1;
+    uint256 private constant MASK_64BITS = (2 ** 64) - 1;
+    uint256 private constant MASK_192BITS = (2 ** 192) - 1;
+
+
+    /**  @dev Verifies and stores a `merkle_root` for specified `block_number`. Calls `verifyProof`
+     * in `PlonkVerifier` and reverts if the proof or the public inputs are malformed.
+     *
+     * @param block_number Block number where merkle root was relayed.
+     * @param merkle_root Merkle root containing messages queued to relay on VARA.
+     * @param proof serialised plonk proof (using gnark's MarshalSolidity).
+     */
+    function submitMerkleRoot(
+        uint256 block_number,
+        bytes32 merkle_root,
+        bytes calldata proof
+    ) external {
+        uint256[] memory public_inputs = _buildPublicInputs(
+            block_number,
+            merkle_root
+        );
+        if (!IVerifier(VERIFIER_ADDRESS).verifyProof(proof, public_inputs)) {
+            revert InvalidProof();
+        }
+
+        _block_numbers[block_number] = merkle_root;
+        _merkle_roots[merkle_root] = block_number;
+
+        emit MerkleRoot(block_number, bytes32(merkle_root));
+    }
+
+    /**
+     * @dev Returns merkle root for specified block number. Returns bytes32(0) if merkle root was
+     * not provided for specified block_number.
+     *
+     * @param block_number Target block number.
+     * @return merkle_root, bytes32(0) if no merkle root was found.
+     */
+    function getMerkleRoot(
+        uint256 block_number
+    ) external view returns (bytes32) {
+        return _block_numbers[block_number];
+    }
+
+    /**
+     * @dev Returns block number for provided merkle_root. Returns uint256(0) if merkle root was not
+     * provided for specified block_number
+     *
+     * @param merkle_root merkle root
+     * @return block_number, uint256(0) if no block number was found.
+     */
+    function getBlockNumber(
+        bytes32 merkle_root
+    ) external view returns (uint256) {
+        return _merkle_roots[merkle_root];
+    }
+
+    /**
+     * @dev Constructs public inputs for verifier from provided `block_number` and `merkle_root`.
+     *
+     * @param block_number Target block number.
+     * @param merkle_root Target merkle root.
+     * @return public_inputs Constructed public inputs.
+     */
+    function buildPublicInputs(
+        uint256 block_number,
+        bytes32 merkle_root
+    ) public pure returns (uint256[] memory public_inputs) {
+        return _buildPublicInputs(block_number, merkle_root);
+    }
+
+    /**
+     * @dev Constructs public inputs for verifier from provided `block_number` and `merkle_root`.
+     *
+     * @param block_number Target block number.
+     * @param merkle_root Target merkle root.
+     * @return public_inputs Constructed public inputs.
+     */
+    function _buildPublicInputs(
+        uint256 block_number,
+        bytes32 merkle_root
+    ) public pure returns (uint256[] memory public_inputs) {
+        uint256[] memory ret = new uint256[](2);
+        ret[0] = (uint256(merkle_root) >> 64) & MASK_192BITS;
+        ret[1] =
+            ((uint256(block_number) & MASK_32BITS) << 96) |
+            ((uint256(merkle_root) & MASK_64BITS) << 128);
+
+        return ret;
+    }
+}
---
+++ ethereum/src/mocks/ERC20Mock.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.24;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Context} from "@openzeppelin/contracts/utils/Context.sol";
+
+contract ERC20Mock is Context, ERC20 {
+    constructor(string memory _symbol) ERC20(_symbol, _symbol) {
+        _mint(_msgSender(), 1000 * (10 ** 18));
+    }
+}

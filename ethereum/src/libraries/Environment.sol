743c84a1029a52b59a15849196c32e9024b5cdde
mertwole <mertwole@gmail.com>
2024-07-11 08:17:17 +0000

    Update ethereum contract addresses

--- ethereum/src/libraries/Environment.sol
+++ ethereum/src/libraries/Environment.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.8.24;
 
 address constant TREASURY_ADDRESS = address(
-    0xa513E6E4b8f2a923D98304ec87F64353C4D5C853
-);
-address constant MESSAGE_QUEUE_ADDRESS = address(
     0x0165878A594ca255338adfa4d48449f69242Eb8F
 );
-address constant RELAYER_ADDRESS = address(
+address constant MESSAGE_QUEUE_ADDRESS = address(
     0x5FC8d32690cc91D4c39d9d3abcBD16989F875707
 );
+address constant RELAYER_ADDRESS = address(
+    0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9
+);
 address constant VERIFIER_ADDRESS = address(
-    0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+    0x5FbDB2315678afecb367f032d93F642f64180aa3
 );
 
 // ALICE

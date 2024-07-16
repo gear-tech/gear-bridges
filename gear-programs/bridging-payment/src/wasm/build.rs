dde08811a7264246a5f5fa73590519027a29e4b9
Louisa <30317856+LouiseMedova@users.noreply.github.com>
2024-07-16 13:08:07 +0300

    Update gear-programs/bridging-payment/src/wasm/build.rs
    Co-authored-by: mertwole <33563701+mertwole@users.noreply.github.com>

--- gear-programs/bridging-payment/src/wasm/build.rs
+++ gear-programs/bridging-payment/src/wasm/build.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let manifest_dir_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    let idl_file_path = manifest_dir_path.join("bridge-payment.idl");
+    let idl_file_path = manifest_dir_path.join("bridging-payment.idl");
 
     let idl_file = File::create(idl_file_path).unwrap();
 

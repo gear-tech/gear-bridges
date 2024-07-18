7f84e9c730d942e6f9ac5ecf9548f274bfddead1
Georgy Shepelev <geo.shepelev@gmail.com>
2024-07-18 10:40:26 +0400

    initial relayer struct

---
+++ checkpoints-relayer/src/main.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+use tokio::sync::mpsc;
+
+const SIZE_CHANNEL: usize = 100_000;
+
+#[derive(Debug, Parser)]
+struct Args {
+    /// Specify ProgramId of the Checkpoint-light-client program
+    #[arg(long)]
+    program_id: String,
+
+    /// Specify an endpoint providing Beacon API
+    #[arg(long)]
+    beacon_endpoint: String,
+
+    /// Address of the VARA RPC endpoint
+    #[arg(
+        long,
+        env = "VARA_RPC"
+    )]
+    vara_endpoint: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let Args {
+        program_id,
+        beacon_endpoint,
+        vara_endpoint,
+    } = Args::parse();
+
+    let (sender, receiver) = mpsc::channel::<()>(SIZE_CHANNEL);
+}

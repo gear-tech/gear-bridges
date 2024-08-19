1bfe708cb767e1b790c0cd416d06eb4548871046
Georgy Shepelev <geo.shepelev@gmail.com>
2024-08-19 12:29:37 +0300

    fix review remarks

--- ethereum-common/src/patricia_trie.rs
+++ ethereum-common/src/patricia_trie.rs
@@ -20,22 +20,6 @@ impl trie_db::TrieLayout for Layout {
 ///
 /// Use it as a `Trie` trait object. You can use `db()` to get the backing database object.
 /// Use `get` and `contains` to query values associated with keys in the trie.
-///
-/// # Example
-/// ```
-/// use hash_db::*;
-/// use memory_db::*;
-/// use trie_db::*;
-/// use ethereum_types::H256;
-/// use ethereum_common::{patricia_trie::{TrieDB, TrieDBMut}, memory_db};
-///
-///   let mut memdb = memory_db::new();
-///   let mut root = H256::zero();
-///   TrieDBMut::new(&mut memdb, &mut root).insert(b"foo", b"bar").unwrap();
-///   let t = TrieDB::new(&memdb, &root).unwrap();
-///   assert!(t.contains(b"foo").unwrap());
-///   assert_eq!(t.get(b"foo").unwrap().unwrap(), b"bar".to_vec());
-/// ```
 pub type TrieDB<'db> = trie_db::TrieDB<'db, Layout>;
 
 /// Convenience type alias to instantiate a Keccak/Rlp-flavoured `TrieDBMut`
@@ -43,26 +27,6 @@ pub type TrieDB<'db> = trie_db::TrieDB<'db, Layout>;
 /// Use it as a `TrieMut` trait object. You can use `db()` to get the backing database object.
 /// Note that changes are not committed to the database until `commit` is called.
 /// Querying the root or dropping the trie will commit automatically.
-
-/// # Example
-/// ```
-/// use hash_db::*;
-/// use memory_db::*;
-/// use trie_db::*;
-/// use ethereum_types::H256;
-/// use ethereum_common::{patricia_trie::{TrieDB, TrieDBMut}, rlp_node_codec, memory_db};
-///
-///   let mut memdb = memory_db::new();
-///   let mut root = H256::zero();
-///   let mut t = TrieDBMut::new(&mut memdb, &mut root);
-///   assert!(t.is_empty());
-///   assert_eq!(*t.root(), rlp_node_codec::HASHED_NULL_NODE);
-///   t.insert(b"foo", b"bar").unwrap();
-///   assert!(t.contains(b"foo").unwrap());
-///   assert_eq!(t.get(b"foo").unwrap().unwrap(), b"bar".to_vec());
-///   t.remove(b"foo").unwrap();
-///   assert!(!t.contains(b"foo").unwrap());
-/// ```
 pub type TrieDBMut<'db> = trie_db::TrieDBMut<'db, Layout>;
 
 #[cfg(test)]

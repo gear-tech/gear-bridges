49195342b151a9db09632accfb7f367ed03bea72
Nikita Yutanov <nikitayutanov@gmail.com>
2024-08-27 16:32:30 +0300

    Add bridging payment address env

--- frontend/src/features/swap/consts/index.ts
+++ frontend/src/features/swap/consts/index.ts
@@ -1,7 +1,11 @@
+import { HexString } from '@gear-js/api';
+
 import { ERC20_TREASURY_ABI, FUNGIBLE_TOKEN_ABI } from './abi';
 import { FUNCTION_NAME, EVENT_NAME } from './eth';
 import { FIELD_NAME, DEFAULT_VALUES, ADDRESS_SCHEMA, ERROR_MESSAGE } from './form';
 import { BridgingPaymentProgram, VftGatewayProgram, VftProgram } from './sails';
+
+const BRIDGING_PAYMENT_CONTRACT_ADDRESS = import.meta.env.VITE_BRIDGING_PAYMENT_CONTRACT_ADDRESS as HexString;
 
 const NETWORK_INDEX = {
   VARA: 0,
@@ -11,6 +15,7 @@ const NETWORK_INDEX = {
 const BALANCE_REFETCH_INTERVAL = 10000;
 
 export {
+  BRIDGING_PAYMENT_CONTRACT_ADDRESS,
   ERC20_TREASURY_ABI,
   FUNGIBLE_TOKEN_ABI,
   FIELD_NAME,

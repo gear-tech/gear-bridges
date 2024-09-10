10b61dacbaf10e4268c38663d6f10f24ed9a552a
Georgy Shepelev <geo.shepelev@gmail.com>
2024-09-10 23:11:33 +0400

    use NoopMemTracker for MemoryDB

--- ethereum-common/src/memory_db.rs
+++ ethereum-common/src/memory_db.rs
@@ -1,7 +1,7 @@
 use super::{keccak_hasher::KeccakHasher, *};
-use ::memory_db::HashKey;
+use ::memory_db::{HashKey, NoopTracker};
 
-pub type MemoryDB = ::memory_db::MemoryDB<KeccakHasher, HashKey<KeccakHasher>, Vec<u8>>;
+pub type MemoryDB = ::memory_db::MemoryDB<KeccakHasher, HashKey<KeccakHasher>, Vec<u8>, NoopTracker<Vec<u8>>>;
 
 pub fn new() -> MemoryDB {
     memory_db::MemoryDB::from_null_node(&rlp::NULL_RLP, rlp::NULL_RLP.as_ref().into())

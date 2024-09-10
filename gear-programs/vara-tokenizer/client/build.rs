d3bd9b2853ac078b89a650f9be937d9da4ed7684
vobradovich <vadik.on@gmail.com>
2024-09-10 13:57:26 +0200

    fix

--- gear-programs/vara-tokenizer/client/build.rs
+++ gear-programs/vara-tokenizer/client/build.rs
@@ -6,7 +6,8 @@ fn main() {
     let idl_file_path = out_dir_path.join("vara_tokenizer.idl");
 
     // Generate IDL file for the program
-    sails_idl_gen::generate_idl_to_file::<vara_tokenizer_app::VaraTokenizerProgram>(&idl_file_path).unwrap();
+    sails_idl_gen::generate_idl_to_file::<vara_tokenizer_app::VaraTokenizerProgram>(&idl_file_path)
+        .unwrap();
 
     // Generate client code from IDL file
     ClientGenerator::from_idl_path(&idl_file_path)

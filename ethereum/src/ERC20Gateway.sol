9b5853a397e4b616acea8527eec79718a567bd0e
mertwole <mertwole@gmail.com>
2024-09-25 08:27:31 +0000

    Remove SafeERC20

--- ethereum/src/ERC20Gateway.sol
+++ ethereum/src/ERC20Gateway.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.24;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Context} from "@openzeppelin/contracts/utils/Context.sol";
 
-import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
 
 import {IERC20Gateway, WithdrawMessage} from "./interfaces/IERC20Gateway.sol";
@@ -12,8 +11,6 @@ import {IMessageQueue, IMessageQueueReceiver, VaraMessage} from "./interfaces/IM
 import {ERC20VaraSupply} from "./ERC20VaraSupply.sol";
 
 contract ERC20Gateway is IERC20Gateway, Context, IMessageQueueReceiver {
-    using SafeERC20 for IERC20; // TODO: Do we need it?
-
     address immutable MESSAGE_QUEUE_ADDRESS;
 
     constructor(address message_queue) {

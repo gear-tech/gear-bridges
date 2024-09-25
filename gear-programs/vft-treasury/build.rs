3e96a0339dd45a253af44e7ebdf28c4ae49a7d73
Roman Maslennikov <m3angreen@gmail.com>
2024-09-25 15:38:46 +0400

    Update treasury

--- gear-programs/vft-treasury/build.rs
+++ gear-programs/vft-treasury/build.rs
@@ -1,24 +1,23 @@
-use sails_client_gen::ClientGenerator;
-use std::{env, path::PathBuf};
+use std::{
+    env,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+};
 
 fn main() {
-    let out_dir_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let client_rs_file_path = out_dir_path.join("vft.rs");
+    sails_rs::build_wasm();
 
-    #[cfg(not(target_family = "windows"))]
-    let idl_file_path = out_dir_path.join("vft.idl");
-    #[cfg(not(target_family = "windows"))]
-    git_download::repo("https://github.com/gear-foundation/standards")
-        .branch_name("master")
-        .add_file("extended-vft/wasm/extended_vft.idl", &idl_file_path)
-        .exec()
-        .unwrap();
+    if env::var("__GEAR_WASM_BUILDER_NO_BUILD").is_ok() {
+        return;
+    }
 
-    // use local copy of `vft.idl` to build on windows
-    #[cfg(target_family = "windows")]
-    let idl_file_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("vft.idl");
+    let bin_path_file = File::open(".binpath").unwrap();
+    let mut bin_path_reader = BufReader::new(bin_path_file);
+    let mut bin_path = String::new();
+    bin_path_reader.read_line(&mut bin_path).unwrap();
 
-    ClientGenerator::from_idl_path(&idl_file_path)
-        .generate_to(client_rs_file_path)
-        .unwrap();
+    let mut idl_path = PathBuf::from(bin_path);
+    idl_path.set_extension("idl");
+    sails_idl_gen::generate_idl_to_file::<vft_treasury_app::Program>(idl_path).unwrap();
 }

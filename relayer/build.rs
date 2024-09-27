62b5a7694441459987c10dc0a310a860580e9653
Roman Maslennikov <m3angreen@gmail.com>
2024-09-27 19:34:37 +0400

    Update relayer

--- relayer/build.rs
+++ relayer/build.rs
@@ -1,9 +1,5 @@
-use sails_client_gen::ClientGenerator;
-use std::{env, path::PathBuf};
-
 fn main() {
     go_bindings();
-    bridging_payment_client();
 }
 
 fn go_bindings() {
@@ -14,19 +10,4 @@ fn go_bindings() {
         .change_dir("./../gnark-wrapper")
         .package("main.go")
         .build("gnark_wrapper");
-}
-
-fn bridging_payment_client() {
-    println!(
-        "cargo:rerun-if-changed=../gear-programs/bridging-payment/src/wasm/bridging-payment.idl"
-    );
-
-    let out_dir_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let idl_file_path =
-        PathBuf::from("../gear-programs/bridging-payment/src/wasm/bridging-payment.idl");
-    let client_rs_file_path = out_dir_path.join("bridging_payment_client.rs");
-
-    ClientGenerator::from_idl_path(&idl_file_path)
-        .generate_to(client_rs_file_path)
-        .unwrap();
 }

18fd443a00daa886bc93d2e813e0654606c95faa
mertwole <mertwole@gmail.com>
2024-10-08 10:51:55 +0000

    Compose relayer

--- relayer/src/message_relayer/eth_to_gear/all_token_transfers.rs
+++ relayer/src/message_relayer/eth_to_gear/all_token_transfers.rs
@@ -1,1 +1,87 @@
-// TODO
+use std::iter;
+
+use primitive_types::{H160, H256};
+
+use ethereum_client::EthApi;
+use gear_rpc_client::GearApi;
+use utils_prometheus::MeteredService;
+
+use crate::message_relayer::common::{
+    self,
+    ethereum::{
+        block_listener::BlockListener as EthereumBlockListener,
+        deposit_event_extractor::DepositEventExtractor, merkle_root_extractor::MerkleRootExtractor,
+    },
+    gear::{
+        block_listener::BlockListener as GearBlockListener,
+        checkpoints_extractor::CheckpointsExtractor,
+        message_queued_event_extractor::MessageQueuedEventExtractor, message_sender::MessageSender,
+    },
+};
+
+pub struct Relayer {
+    gear_block_listener: GearBlockListener,
+    ethereum_block_listener: EthereumBlockListener,
+
+    deposit_event_extractor: DepositEventExtractor,
+    checkpoints_extractor: CheckpointsExtractor,
+
+    gear_message_sender: MessageSender,
+}
+
+impl MeteredService for Relayer {
+    fn get_sources(&self) -> impl IntoIterator<Item = Box<dyn prometheus::core::Collector>> {
+        iter::empty()
+            .chain(self.gear_block_listener.get_sources())
+            .chain(self.ethereum_block_listener.get_sources())
+            .chain(self.deposit_event_extractor.get_sources())
+            .chain(self.checkpoints_extractor.get_sources())
+            .chain(self.gear_message_sender.get_sources())
+    }
+}
+
+impl Relayer {
+    pub async fn new(
+        gear_api: GearApi,
+        eth_api: EthApi,
+        erc20_treasury_address: H160,
+        checkpoint_light_client_address: H256,
+    ) -> anyhow::Result<Self> {
+        let from_eth_block = eth_api.block_number().await?;
+
+        let from_gear_block = gear_api.latest_finalized_block().await?;
+        let from_gear_block = gear_api.block_hash_to_number(block).await?;
+
+        let gear_block_listener = GearBlockListener::new(gear_api.clone(), from_gear_block);
+
+        let ethereum_block_listener = EthereumBlockListener::new(eth_api.clone(), from_eth_block);
+
+        let deposit_event_extractor =
+            DepositEventExtractor::new(eth_api.clone(), erc20_treasury_address);
+
+        let checkpoints_extractor =
+            CheckpointsExtractor::new(gear_api.clone(), checkpoint_light_client_address);
+
+        let gear_message_sender = MessageSender::new(gear_api);
+
+        Ok(Self {
+            gear_block_listener,
+            ethereum_block_listener,
+
+            deposit_event_extractor,
+            checkpoints_extractor,
+
+            gear_message_sender,
+        })
+    }
+
+    pub fn run(self) {
+        let [gear_blocks] = self.gear_block_listener.run();
+        let ethereum_blocks = self.ethereum_block_listener.run();
+
+        let deposit_events = self.deposit_event_extractor.run(ethereum_blocks);
+        let checkpoints = self.checkpoints_extractor.run(gear_blocks);
+
+        self.gear_message_sender.run(deposit_events, checkpoints);
+    }
+}

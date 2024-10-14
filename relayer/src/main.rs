61b3461f662a81c019d61ed9da5a958fe96a29ac
mertwole <mertwole@gmail.com>
2024-10-14 10:42:02 +0000

    Compose cli args

--- relayer/src/main.rs
+++ relayer/src/main.rs
@@ -3,11 +3,14 @@ extern crate pretty_env_logger;
 use std::time::Duration;
 
 use clap::{Args, Parser, Subcommand};
-use message_relayer::gear_to_eth::{all_token_transfers, paid_token_transfers};
+use ethereum_beacon_client::BeaconClient;
+use gclient::{GearApi as GclientGearApi, WSAddress};
 use pretty_env_logger::env_logger::fmt::TimestampPrecision;
+use primitive_types::{H160, H256};
 
 use ethereum_client::EthApi;
 use gear_rpc_client::GearApi;
+use message_relayer::{eth_to_gear, gear_to_eth};
 use proof_storage::{FileSystemProofStorage, GearProofStorage, ProofStorage};
 use relay_merkle_roots::MerkleRootRelayer;
 use utils_prometheus::MetricsBuilder;
@@ -21,6 +24,7 @@ mod prover_interface;
 mod relay_merkle_roots;
 
 const DEFAULT_VARA_RPC: &str = "ws://localhost:8989";
+const DEFAULT_ETH_BEACON_RPC: &str = "http://localhost:50000";
 const DEFAULT_ETH_RPC: &str = "http://localhost:8545";
 const DEFAULT_PROMETHEUS_ENDPOINT: &str = "0.0.0.0:9090";
 
@@ -89,6 +93,7 @@ struct FetchAuthoritySetStateArgs {
     write_to_file: bool,
 }
 
+// TODO: Separate domain and port.
 #[derive(Args)]
 struct VaraEndpointArg {
     /// Address of the VARA RPC endpoint
@@ -118,6 +123,21 @@ struct EthereumArgs {
     /// Ethereum address of message queue contract
     #[arg(long = "mq-address", env = "ETH_MESSAGE_QUEUE_ADDRESS")]
     mq_address: String,
+}
+
+#[derive(Args)]
+struct BeaconRpcArgs {
+    /// Address of the ethereum beacon RPC endpoint
+    #[arg(
+        long = "ethereum-beacon-rpc",
+        default_value = DEFAULT_ETH_BEACON_RPC,
+        env = "ETH_BEACON_RPC"
+    )]
+    endpoint: String,
+
+    /// Timeout in seconds for requests to the ethereum beacon RPC
+    #[arg(long = "ethereum-beacon-rpc-timeout", env = "ETH_BEACON_RPC_TIMEOUT")]
+    timeout: Option<u64>,
 }
 
 #[derive(Args)]
@@ -173,40 +193,42 @@ struct RelayCheckpointsArgs {
 
 #[derive(Args)]
 struct RelayErc20Args {
-    /// Specify ProgramId of the program
-    #[arg(long, env = "ADDRESS")]
-    program_id: String,
+    /// Address of the ERC20Treasury contract on ethereum
+    #[arg(long = "erc20-treasury-address", env = "ERC20_TREASURY_ADDRESS")]
+    erc20_treasury_address: String,
 
-    /// Specify an endpoint providing Beacon API
-    #[arg(long, env = "BEACON_ENDPOINT")]
-    beacon_endpoint: String,
+    /// Address of the checkpoint-light-client program on gear
+    #[arg(
+        long = "checkpoint-light-client-address",
+        env = "CHECKPOINT_LIGHT_CLIENT_ADDRESS"
+    )]
+    checkpoint_light_client_address: String,
 
-    /// Domain of the VARA RPC endpoint
-    #[arg(long, default_value = "ws://127.0.0.1", env = "VARA_DOMAIN")]
-    vara_domain: String,
+    /// Address of the ethereum-event-client program on gear
+    #[arg(
+        long = "ethereum-event-client-address",
+        env = "ETHEREUM_EVENT_CLIENT_ADDRESS"
+    )]
+    ethereum_event_client_address: String,
 
-    /// Port of the VARA RPC endpoint
-    #[arg(long, default_value = "9944", env = "VARA_PORT")]
-    vara_port: u16,
+    #[clap(flatten)]
+    vara_endpoint: VaraEndpointArg,
 
     /// Substrate URI that identifies a user by a mnemonic phrase or
     /// provides default users from the keyring (e.g., "//Alice", "//Bob",
     /// etc.). The password for URI should be specified in the same `suri`,
     /// separated by the ':' char
-    #[arg(long, default_value = "//Alice", env = "VARA_SURI")]
+    #[arg(long, env = "VARA_SURI")]
     vara_suri: String,
 
-    /// Address of the ethereum endpoint
-    #[arg(
-        long = "ethereum-endpoint",
-        default_value = DEFAULT_ETH_RPC,
-        env = "ETH_RPC"
-    )]
-    eth_endpoint: String,
+    #[clap(flatten)]
+    ethereum_args: EthereumArgs,
 
-    /// Specify the hash of the ERC20-transaction to relay
-    #[arg(long, env = "TX_HASH")]
-    tx_hash: String,
+    #[clap(flatten)]
+    beacon_rpc: BeaconRpcArgs,
+
+    #[clap(flatten)]
+    prometheus_args: PrometheusArgs,
 }
 
 #[tokio::main]
@@ -269,20 +291,9 @@ async fn main() {
             let eth_api = create_eth_client(&args.ethereum_args);
 
             if let Some(bridging_payment_address) = args.bridging_payment_address {
-                let bridging_payment_address = if &bridging_payment_address[..2] == "0x" {
-                    &bridging_payment_address[2..]
-                } else {
-                    &bridging_payment_address
-                };
+                let bridging_payment_address = decode_h256(&bridging_payment_address);
 
-                let bridging_payment_address: [u8; 32] = hex::decode(bridging_payment_address)
-                    .expect("Wrong format of bridging-payment-address")
-                    .try_into()
-                    .expect("Wrong format of bridging-payment-address");
-
-                let bridging_payment_address = bridging_payment_address.into();
-
-                let relayer = paid_token_transfers::Relayer::new(
+                let relayer = gear_to_eth::paid_token_transfers::Relayer::new(
                     gear_api,
                     eth_api,
                     args.from_block,
@@ -299,9 +310,13 @@ async fn main() {
 
                 relayer.run();
             } else {
-                let relayer = all_token_transfers::Relayer::new(gear_api, eth_api, args.from_block)
-                    .await
-                    .unwrap();
+                let relayer = gear_to_eth::all_token_transfers::Relayer::new(
+                    gear_api,
+                    eth_api,
+                    args.from_block,
+                )
+                .await
+                .unwrap();
 
                 MetricsBuilder::new()
                     .register_service(&relayer)
@@ -324,8 +339,58 @@ async fn main() {
                 .await
                 .expect("Failed to fetch authority set state");
         }
-        CliCommands::RelayErc20(_args) => todo!(),
+        CliCommands::RelayErc20(args) => {
+            let eth_api = create_eth_client(&args.ethereum_args);
+            let beacon_client = create_beacon_client(&args.beacon_rpc).await;
+
+            let gear_api = create_gear_client(&args.vara_endpoint).await;
+            let gclient_client = create_gclient_client(&args.vara_endpoint, &args.vara_suri).await;
+
+            let erc20_treasury_address = decode_h160(&args.erc20_treasury_address);
+            let checkpoint_light_client_address =
+                decode_h256(&args.checkpoint_light_client_address);
+            let ethereum_event_client_address = decode_h256(&args.ethereum_event_client_address);
+
+            let relayer = eth_to_gear::all_token_transfers::Relayer::new(
+                gear_api,
+                gclient_client,
+                eth_api,
+                beacon_client,
+                erc20_treasury_address,
+                checkpoint_light_client_address,
+                ethereum_event_client_address,
+            )
+            .await
+            .expect("Failed to create relayer");
+
+            MetricsBuilder::new()
+                .register_service(&relayer)
+                .build()
+                .run(args.prometheus_args.endpoint)
+                .await;
+
+            relayer.run();
+
+            loop {
+                // relayer.run() spawns thread and exits, so we need to add this loop after calling run.
+                std::thread::sleep(Duration::from_millis(100));
+            }
+        }
     };
+}
+
+async fn create_gclient_client(args: &VaraEndpointArg, vara_suri: &str) -> GclientGearApi {
+    let endpoint_parts: Vec<_> = args.vara_endpoint.split(':').collect();
+    let [domain_1, domain_2, port] = endpoint_parts
+        .try_into()
+        .expect("Invalid gear endpoint provided");
+
+    let domain = [domain_1, domain_2].join(":");
+    let port: u16 = port.parse().expect("Invalid gear endpoint provided");
+
+    GclientGearApi::init_with(WSAddress::new(domain, Some(port)), vara_suri)
+        .await
+        .expect("Failed to create gclient client")
 }
 
 async fn create_gear_client(args: &VaraEndpointArg) -> GearApi {
@@ -349,4 +414,31 @@ fn create_eth_client(args: &EthereumArgs) -> EthApi {
         fee_payer.as_deref(),
     )
     .unwrap_or_else(|err| panic!("Error while creating ethereum client: {}", err))
+}
+
+async fn create_beacon_client(args: &BeaconRpcArgs) -> BeaconClient {
+    let timeout = args.timeout.map(Duration::from_secs);
+
+    BeaconClient::connect(args.endpoint.clone(), timeout)
+        .await
+        .expect("Failed to create beacon client")
+}
+
+fn decode_h256(hex: &str) -> H256 {
+    let data: [u8; 32] = decode_byte_array(hex);
+    data.into()
+}
+
+fn decode_h160(hex: &str) -> H160 {
+    let data: [u8; 20] = decode_byte_array(hex);
+    data.into()
+}
+
+fn decode_byte_array<const LEN: usize>(hex: &str) -> [u8; LEN] {
+    let address = if &hex[..2] == "0x" { &hex[2..] } else { hex };
+
+    hex::decode(address)
+        .unwrap_or_else(|_| panic!("Wrong address format: {}", hex))
+        .try_into()
+        .unwrap_or_else(|_| panic!("Wrong address length: {}", hex))
 }

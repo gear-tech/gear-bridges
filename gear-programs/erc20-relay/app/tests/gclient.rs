d0261ce40688bbc7ee95817f005232345bd2541f
Georgy Shepelev <geo.shepelev@gmail.com>
2024-10-14 19:25:13 +0400

    initial

--- gear-programs/erc20-relay/app/tests/gclient.rs
+++ gear-programs/erc20-relay/app/tests/gclient.rs
@@ -4,10 +4,58 @@ mod vft {
     include!(concat!(env!("OUT_DIR"), "/vft-gateway.rs"));
 }
 
-use erc20_relay_client::traits::{Erc20Relay, Erc20RelayFactory};
+use ethereum_common::{utils as eth_utils, memory_db, trie_db::{TrieMut, Trie, Recorder}, patricia_trie::{TrieDB, TrieDBMut}};
+use erc20_relay_client::{EthToVaraEvent, BlockInclusionProof, traits::{Erc20Relay, Erc20RelayFactory}};
 use gclient::{Event, EventProcessor, GearApi, GearEvent};
 use sails_rs::{calls::*, gclient::calls::*, prelude::*};
 use vft::vft_gateway;
+use alloy_rlp::Encodable;
+use alloy::rpc::types::{Log, TransactionReceipt};
+use alloy_primitives::Log as PrimitiveLog;
+use alloy_consensus::{TxType, Receipt, ReceiptWithBloom, ReceiptEnvelope};
+use serde::Deserialize;
+
+const HOLESKY_RECEIPTS_2_498_456: &[u8; 160_144] =
+    include_bytes!("./holesky-receipts-2_498_456.json");
+const HOLESKY_BLOCK_2_498_456: &[u8; 235_397] = include_bytes!("./holesky-block-2_498_456.json");
+const HOLESKY_HEADER_2_498_457: &[u8; 670] = include_bytes!("./holesky-header-2_498_457.json");
+const HOLESKY_HEADER_2_498_458: &[u8; 669] = include_bytes!("./holesky-header-2_498_458.json");
+const HOLESKY_HEADER_2_498_459: &[u8; 670] = include_bytes!("./holesky-header-2_498_459.json");
+const HOLESKY_HEADER_2_498_460: &[u8; 670] = include_bytes!("./holesky-header-2_498_460.json");
+const HOLESKY_HEADER_2_498_461: &[u8; 670] = include_bytes!("./holesky-header-2_498_461.json");
+const HOLESKY_HEADER_2_498_462: &[u8; 669] = include_bytes!("./holesky-header-2_498_462.json");
+const HOLESKY_HEADER_2_498_463: &[u8; 670] = include_bytes!("./holesky-header-2_498_463.json");
+const HOLESKY_HEADER_2_498_464: &[u8; 669] = include_bytes!("./holesky-header-2_498_464.json");
+
+#[derive(Deserialize)]
+pub struct Receipts {
+    result: Vec<TransactionReceipt>,
+}
+
+fn map_receipt_envelope(receipt: &ReceiptEnvelope<Log>) -> ReceiptEnvelope<PrimitiveLog> {
+    let logs = receipt
+        .logs()
+        .iter()
+        .map(AsRef::as_ref)
+        .cloned()
+        .collect();
+
+    let result = ReceiptWithBloom::new(
+        Receipt {
+            status: receipt.status().into(),
+            cumulative_gas_used: receipt.cumulative_gas_used(),
+            logs,
+        },
+        *receipt.logs_bloom(),
+    );
+
+    match receipt.tx_type() {
+        TxType::Legacy => ReceiptEnvelope::Legacy(result),
+        TxType::Eip1559 => ReceiptEnvelope::Eip1559(result),
+        TxType::Eip2930 => ReceiptEnvelope::Eip2930(result),
+        TxType::Eip4844 => ReceiptEnvelope::Eip4844(result),
+    }
+}
 
 async fn spin_up_node() -> (GClientRemoting, CodeId, GasUnit) {
     let api = GearApi::dev().await.unwrap();
@@ -244,4 +292,59 @@ async fn update_config() {
         },
         client.config().recv(program_id).await.unwrap()
     );
+}
+
+// block = 2_498_456 (number = 2_301_317),
+// checkpoint = 2_498_464 (0xb89c6d200193f865b85a3f323b75d2b10346564a330229d8a5c695968206faf1)
+// tx 0x180cd2328df9c4356adc77e19e33c5aa2d5395f1b52e70d22c25070a04f16691
+
+#[tokio::test]
+#[ignore = "Requires running node"]
+async fn test_relay_erc20() {
+    let receipts: Receipts = serde_json::from_slice(HOLESKY_RECEIPTS_2_498_456.as_ref()).unwrap();
+    let receipts = receipts.result.iter().map(|tx_receipt| {
+        let receipt = tx_receipt.as_ref();
+
+        tx_receipt
+            .transaction_index
+            .map(|i| (i, map_receipt_envelope(receipt)))
+    }).collect::<Option<Vec<_>>>()
+    .unwrap_or_default();
+
+    let mut memory_db = memory_db::new();
+    let key_value_tuples = eth_utils::rlp_encode_receipts_and_nibble_tuples(&receipts[..]);
+    let root = {
+        let mut root = H256::zero();
+        let mut triedbmut = TrieDBMut::new(&mut memory_db, &mut root);
+        for (key, value) in &key_value_tuples {
+            triedbmut.insert(key, value).unwrap();
+        }
+
+        *triedbmut.root()
+    };
+
+    let tx_index = 15;
+    let (tx_index, receipt) = receipts
+        .iter()
+        .find(|(index, _)| index == &tx_index)
+        .unwrap();
+
+    let trie = TrieDB::new(&memory_db, &root).unwrap();
+    let (key, _expected_value) = eth_utils::rlp_encode_index_and_receipt(tx_index, receipt);
+
+    let mut recorder = Recorder::new();
+    let _value = trie.get_with(&key, &mut recorder);
+
+    let mut receipt_rlp = Vec::with_capacity(Encodable::length(receipt));
+    Encodable::encode(receipt, &mut receipt_rlp);
+    // let message = EthToVaraEvent {
+    //     proof_block,
+    //     proof: recorder
+    //         .drain()
+    //         .into_iter()
+    //         .map(|r| r.data)
+    //         .collect::<Vec<_>>(),
+    //     transaction_index: *tx_index,
+    //     receipt_rlp,
+    // };
 }

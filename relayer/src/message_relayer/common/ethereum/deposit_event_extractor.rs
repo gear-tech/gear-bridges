34c9e7a15f1908fa5589a512a28891fde9c46f25
mertwole <mertwole@gmail.com>
2024-10-21 09:13:57 +0000

    Add temporary fix for error Block not found

--- relayer/src/message_relayer/common/ethereum/deposit_event_extractor.rs
+++ relayer/src/message_relayer/common/ethereum/deposit_event_extractor.rs
@@ -1,4 +1,7 @@
-use std::sync::mpsc::{channel, Receiver, Sender};
+use std::{
+    sync::mpsc::{channel, Receiver, Sender},
+    time::Duration,
+};
 
 use alloy::providers::Provider;
 use alloy_eips::BlockNumberOrTag;
@@ -86,29 +89,11 @@ impl DepositEventExtractor {
             .fetch_deposit_events(self.erc20_treasury_address, block.0)
             .await?;
 
-        let block_body = self
-            .eth_api
-            .raw_provider()
-            .get_block_by_number(BlockNumberOrTag::Number(block.0), false)
-            .await?
-            .ok_or(anyhow!("Ethereum block #{} is missing", block.0))?;
+        if events.is_empty() {
+            return Ok(());
+        }
 
-        let beacon_root_parent = block_body.header.parent_beacon_block_root.ok_or(anyhow!(
-            "Unable to determine root of parent beacon block for block #{}",
-            block.0
-        ))?;
-
-        let beacon_block_parent = self
-            .beacon_client
-            .get_block_by_hash(&beacon_root_parent.0)
-            .await?;
-
-        let beacon_block = self
-            .beacon_client
-            .find_beacon_block(block.0, &beacon_block_parent)
-            .await?;
-
-        let slot_number = EthereumSlotNumber(beacon_block.slot);
+        let slot_number = self.find_slot_by_block_number(block).await?;
 
         self.metrics
             .total_deposits_found
@@ -134,5 +119,56 @@ impl DepositEventExtractor {
         }
 
         Ok(())
+    }
+
+    async fn find_slot_by_block_number(
+        &self,
+        block: EthereumBlockNumber,
+    ) -> anyhow::Result<EthereumSlotNumber> {
+        let block_body = self
+            .eth_api
+            .raw_provider()
+            .get_block_by_number(BlockNumberOrTag::Number(block.0), false)
+            .await?
+            .ok_or(anyhow!("Ethereum block #{} is missing", block.0))?;
+
+        let beacon_root_parent = block_body.header.parent_beacon_block_root.ok_or(anyhow!(
+            "Unable to determine root of parent beacon block for block #{}",
+            block.0
+        ))?;
+
+        let beacon_block_parent = self
+            .beacon_client
+            .get_block_by_hash(&beacon_root_parent.0)
+            .await?;
+
+        // TODO: It's a temporary solution of a problem that we're connecting to a different
+        // nodes, so if we're observing finalized block on one node, the finalized slot might still be not
+        // available on other.
+        for _ in 0..10 {
+            let beacon_block_result = self
+                .beacon_client
+                .find_beacon_block(block.0, &beacon_block_parent)
+                .await;
+
+            match beacon_block_result {
+                Ok(beacon_block) => {
+                    return Ok(EthereumSlotNumber(beacon_block.slot));
+                }
+                Err(err) => {
+                    log::warn!(
+                        "Failed to find beacon block for ethereum block #{}: {}. Waiting for 1 second before next attempt...",
+                        block.0,
+                        err
+                    );
+                    tokio::time::sleep(Duration::from_secs(1)).await;
+                }
+            }
+        }
+
+        anyhow::bail!(
+            "Failed to find beacon block for Ethereum block #{} after 5 attempts",
+            block.0
+        );
     }
 }

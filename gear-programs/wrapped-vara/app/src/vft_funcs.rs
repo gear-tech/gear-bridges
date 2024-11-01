2b8191a474b7cf2554fb4564791b3362deb25d6e
Vadim Obradovich <vadik.on@gmail.com>
2024-11-01 11:43:44 +0100

    feat(gear-programs): Wrapped Vara (tokenizer) program extends VftService (#129)

---
+++ gear-programs/wrapped-vara/app/src/vft_funcs.rs
@@ -0,0 +1,44 @@
+use sails_rs::prelude::*;
+use vft_service::{
+    funcs,
+    utils::{Error, Result},
+    Storage,
+};
+
+pub fn mint(to: ActorId, value: U256) -> Result<()> {
+    let total_supply = Storage::total_supply();
+    let balances = Storage::balances();
+
+    let new_total_supply = total_supply
+        .checked_add(value)
+        .ok_or(Error::NumericOverflow)?;
+
+    let new_to = funcs::balance_of(balances, to)
+        .checked_add(value)
+        .ok_or(Error::NumericOverflow)?;
+
+    balances.insert(to, new_to);
+    *total_supply = new_total_supply;
+
+    Ok(())
+}
+
+pub fn burn(from: ActorId, value: U256) -> Result<()> {
+    let total_supply = Storage::total_supply();
+    let balances = Storage::balances();
+
+    let new_total_supply = total_supply.checked_sub(value).ok_or(Error::Underflow)?;
+
+    let new_from = funcs::balance_of(balances, from)
+        .checked_sub(value)
+        .ok_or(Error::InsufficientBalance)?;
+
+    if !new_from.is_zero() {
+        balances.insert(from, new_from);
+    } else {
+        balances.remove(&from);
+    }
+
+    *total_supply = new_total_supply;
+    Ok(())
+}

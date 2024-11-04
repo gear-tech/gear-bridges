77114cc29648e4385f4647b0f121077478ff616f
mertwole <mertwole@gmail.com>
2024-11-04 07:09:36 +0000

    Cleanup

--- ethereum/src/interfaces/IERC20Manager.sol
+++ ethereum/src/interfaces/IERC20Manager.sol
@@ -1,11 +1,5 @@
 pragma solidity ^0.8.24;
 
-struct BridgingRequest {
-    address receiver;
-    address token;
-    uint128 amount;
-}
-
 interface IERC20Manager {
     error NotAuthorized();
     error BadArguments();
@@ -18,18 +12,10 @@ interface IERC20Manager {
         address indexed token,
         uint256 amount
     );
+
     event BridgingAccepted(
         address indexed to,
         address indexed token,
         uint256 amount
     );
-}
-
-library Packer {
-    function pack(
-        BridgingRequest calldata message
-    ) external pure returns (bytes memory) {
-        return
-            abi.encodePacked(message.receiver, message.token, message.amount);
-    }
 }

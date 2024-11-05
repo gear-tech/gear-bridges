f590ff9e00c049d4d3481b668465f1062fb6f336
mertwole <33563701+mertwole@users.noreply.github.com>
2024-11-05 19:18:12 +0400

    feat(ethereum contracts): Introduce `ERC20Manager` (#194)

---
+++ ethereum/src/interfaces/IERC20Manager.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.24;
+
+interface IERC20Manager {
+    error NotAuthorized();
+    error BadArguments();
+    error BadVftManagerAddress();
+    error UnsupportedTokenSupply();
+
+    event BridgingRequested(
+        address indexed from,
+        bytes32 indexed to,
+        address indexed token,
+        uint256 amount
+    );
+
+    event BridgingAccepted(
+        address indexed to,
+        address indexed token,
+        uint256 amount
+    );
+
+    enum SupplyType {
+        Unknown,
+        Ethereum,
+        Gear
+    }
+}

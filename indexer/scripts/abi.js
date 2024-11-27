50adf166376a31bcda3591868d8cbd2f1165bee3
Dmitry Osipov <dmith.osipov@gmail.com>
2024-11-27 11:17:34 +0100

    link api files

--- indexer/scripts/abi.js
+++ indexer/scripts/abi.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const abiPath = process.argv[2];
+const [abiPath, destPath] = process.argv.slice(2);
 
 const abi = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
 
-fs.writeFileSync(path.join('./assets', abiPath.split('/').at(-1)), JSON.stringify(abi.abi, null, 2));
+fs.writeFileSync(path.join(destPath, abiPath.split('/').at(-1)), JSON.stringify(abi.abi, null, 2));

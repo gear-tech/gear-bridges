72cd9e703d1afe09d6a16c2afb5164debe229f95
Nikita Yutanov <nikitayutanov@gmail.com>
2024-11-28 12:35:02 +0300

    chore(frontend): bump api (#214)

--- frontend/src/features/wallet/components/index.ts
+++ frontend/src/features/wallet/components/index.ts
@@ -1,5 +1,4 @@
 import { NetworkWalletField } from './network-wallet-field';
 import { SwapNetworkButton } from './swap-network-button';
-import { Wallet } from './wallet';
 
-export { Wallet, NetworkWalletField, SwapNetworkButton };
+export { NetworkWalletField, SwapNetworkButton };

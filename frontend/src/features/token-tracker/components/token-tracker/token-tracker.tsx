7d1fa86d60d27206301948e09ef7529fedb2411d
Nikita Yutanov <nikitayutanov@gmail.com>
2024-12-05 18:12:07 +0300

    feat(frontend): add locked balance tooltip (#235)

--- frontend/src/features/token-tracker/components/token-tracker/token-tracker.tsx
+++ frontend/src/features/token-tracker/components/token-tracker/token-tracker.tsx
@@ -1,9 +1,13 @@
 import { useAccount } from '@gear-js/react-hooks';
 
+import { Tooltip } from '@/components';
 import { WRAPPED_VARA_CONTRACT_ADDRESS } from '@/consts';
 import { useVaraFTBalance, useEthAccount, useModal, useTokens } from '@/hooks';
 
+import WarningSVG from '../../assets/warning.svg?react';
 import { TokenTrackerModal } from '../token-tracker-modal';
+
+import styles from './token-tracker.module.scss';
 
 function TokenTracker() {
   const { account } = useAccount();
@@ -18,15 +22,17 @@ function TokenTracker() {
 
   return (
     <>
-      <button type="button" onClick={open}>
-        My Tokens
-      </button>
+      <div className={styles.container}>
+        <button type="button" onClick={open}>
+          My Tokens
+        </button>
 
-      {isOpen && (
-        // TODO: remove assertion after @gear-js/vara-ui heading is updated to accept ReactNode.
-        // fast fix for now, cuz major font update was made without a fallback,
-        <TokenTrackerModal lockedBalance={varaLockedBalance} close={close} />
-      )}
+        {Boolean(varaLockedBalance.value) && (
+          <Tooltip SVG={WarningSVG} text="You have tokens available to unlock" position="bottom-end" />
+        )}
+      </div>
+
+      {isOpen && <TokenTrackerModal lockedBalance={varaLockedBalance} close={close} />}
     </>
   );
 }

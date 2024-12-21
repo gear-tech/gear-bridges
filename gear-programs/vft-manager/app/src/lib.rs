55979843cec2a82ba9cc421a9d9cf900654838e3
mertwole <mertwole@gmail.com>
2024-12-21 11:59:24 +0000

    More doc-comments

--- gear-programs/vft-manager/app/src/lib.rs
+++ gear-programs/vft-manager/app/src/lib.rs
@@ -4,16 +4,20 @@ use sails_rs::{gstd::GStdExecContext, prelude::*};
 pub mod services;
 use services::{InitConfig, VftManager};
 
+/// VFT Manager program.
 #[derive(Default)]
 pub struct Program;
 
+/// VFT Manager program implementation.
 #[program]
 impl Program {
+    /// Create VFT Manager program.
     pub fn new(init_config: InitConfig) -> Self {
         VftManager::<GStdExecContext>::seed(init_config, GStdExecContext::new());
         Self
     }
 
+    /// Get VFT Manager service.
     pub fn vft_manager(&self) -> VftManager<GStdExecContext> {
         VftManager::new(GStdExecContext::new())
     }

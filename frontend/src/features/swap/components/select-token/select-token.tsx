fe81182030088e57e29f2d2417882b314bf73403
Nikita Yutanov <nikitayutanov@gmail.com>
2025-03-29 23:09:28 +0300

    add search to select token modal

--- frontend/src/features/swap/components/select-token/select-token.tsx
+++ frontend/src/features/swap/components/select-token/select-token.tsx
@@ -1,6 +1,9 @@
-import { Modal } from '@gear-js/vara-ui';
+import { HexString } from '@gear-js/api';
+import { Input, Modal } from '@gear-js/vara-ui';
 import { isUndefined } from '@polkadot/util';
+import { useState } from 'react';
 
+import SearchSVG from '@/assets/search.svg?react';
 import TokenPlaceholderSVG from '@/assets/token-placeholder.svg?react';
 import { FormattedBalance, Skeleton } from '@/components';
 import { TOKEN_SVG, WRAPPED_VARA_CONTRACT_ADDRESS } from '@/consts';
@@ -27,26 +30,50 @@ type ModalProps = Pick<Props, 'isVaraNetwork' | 'pairIndex' | 'accountBalance' |
 
 function SelectTokenModal({ isVaraNetwork, pairIndex, accountBalance, onChange, close }: ModalProps) {
   const { addresses, symbols, decimals } = useTokens();
-
   const varaFtBalances = useVaraFTBalances(addresses);
   const ethFtBalances = useEthFTBalances(addresses);
-  const ftBalances = isVaraNetwork ? varaFtBalances : ethFtBalances;
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const renderTokenBalance = (address: HexString) => {
+    const ftBalances = isVaraNetwork ? varaFtBalances : ethFtBalances;
+    const isNativeToken = address === WRAPPED_VARA_CONTRACT_ADDRESS;
+    const ftBalance = { data: ftBalances.data?.[address], isLoading: ftBalances.isLoading };
+    const balance = isNativeToken ? getMergedBalance(accountBalance, ftBalance) : ftBalance;
+
+    if (!decimals || balance.isLoading) return <Skeleton width="5rem" />;
+    if (isUndefined(balance.data)) return;
+
+    return <FormattedBalance value={balance.data} symbol="" decimals={decimals[address]} className={styles.balance} />;
+  };
+
+  const filteredAddresses =
+    addresses && symbols
+      ? addresses.filter(([varaAddress, ethAddress]) => {
+          const address = isVaraNetwork ? varaAddress : ethAddress;
+          const lowerCaseSymbol = symbols[address].toLocaleLowerCase();
+          const lowerCaseSearchQuery = searchQuery.toLocaleLowerCase();
+
+          return lowerCaseSymbol.includes(lowerCaseSearchQuery);
+        })
+      : undefined;
 
   const renderTokens = () => {
-    if (!addresses || !symbols) return;
+    if (!addresses || !filteredAddresses || !symbols) return;
 
-    return addresses.map(([varaAddress, ethAddress], index) => {
+    const selectedTokenAddress = addresses[pairIndex][isVaraNetwork ? 0 : 1];
+
+    return filteredAddresses.map(([varaAddress, ethAddress], index) => {
       const address = isVaraNetwork ? varaAddress : ethAddress;
-
-      const isActive = index === pairIndex;
+      const isActive = address === selectedTokenAddress;
       const SVG = TOKEN_SVG[address] ?? TokenPlaceholderSVG;
-
-      const isNativeToken = address === WRAPPED_VARA_CONTRACT_ADDRESS;
-      const ftBalance = { data: ftBalances.data?.[address], isLoading: ftBalances.isLoading };
-      const balance = isNativeToken ? getMergedBalance(accountBalance, ftBalance) : ftBalance;
+      const symbol = symbols[address];
+      const network = isVaraNetwork ? 'Vara' : 'Ethereum';
 
       const handleClick = () => {
-        onChange(index);
+        const indexWithinNonFilteredAddresses = addresses.findIndex((pair) => pair[isVaraNetwork ? 0 : 1] === address);
+
+        onChange(indexWithinNonFilteredAddresses);
         close();
       };
 
@@ -61,23 +88,12 @@ function SelectTokenModal({ isVaraNetwork, pairIndex, accountBalance, onChange, 
               <SVG />
 
               <span className={styles.token}>
-                <span className={styles.symbol}>{symbols[address]}</span>
-                <span className={styles.network}>{isVaraNetwork ? 'Vara' : 'Ethereum'}</span>
+                <span className={styles.symbol}>{symbol}</span>
+                <span className={styles.network}>{network}</span>
               </span>
             </span>
 
-            {!decimals || balance.isLoading ? (
-              <Skeleton width="5rem" />
-            ) : (
-              !isUndefined(balance.data) && (
-                <FormattedBalance
-                  value={balance.data}
-                  symbol=""
-                  decimals={decimals[address]}
-                  className={styles.balance}
-                />
-              )
-            )}
+            {renderTokenBalance(address)}
           </button>
         </li>
       );
@@ -86,7 +102,18 @@ function SelectTokenModal({ isVaraNetwork, pairIndex, accountBalance, onChange, 
 
   return (
     <Modal heading="Select Token" maxWidth="490px" close={close}>
-      <ul className={styles.tokens}>{renderTokens()}</ul>
+      <Input
+        label="Token Name"
+        icon={SearchSVG}
+        onChange={({ target }) => setSearchQuery(target.value)}
+        className={styles.input}
+      />
+
+      {filteredAddresses?.length ? (
+        <ul className={styles.tokens}>{renderTokens()}</ul>
+      ) : (
+        <p className={styles.notFound}>Tokens with provided name are not found.</p>
+      )}
     </Modal>
   );
 }

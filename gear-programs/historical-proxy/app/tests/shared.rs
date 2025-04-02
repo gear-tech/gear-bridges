c60f68ef8c1b3ccb6f00639a7ca05da01dc6610f
Georgy Shepelev <geo.shepelev@gmail.com>
2025-04-02 15:50:42 +0400

    restore historical-proxy tests

---
+++ gear-programs/historical-proxy/app/tests/shared.rs
@@ -0,0 +1,134 @@
+use alloy::rpc::types::TransactionReceipt;
+use alloy_rlp::Encodable;
+use ethereum_common::{
+    beacon::Block,
+    utils::{self as eth_utils, BeaconBlockHeaderResponse, BeaconBlockResponse, MerkleProof},
+};
+use eth_events_deneb_client::{BlockInclusionProof, EthToVaraEvent, BlockGenericForBlockBody};
+use serde::Deserialize;
+
+pub const HOLESKY_RECEIPTS_2_498_456: &[u8; 160_144] =
+    include_bytes!("./headers/holesky-receipts-2_498_456.json");
+pub const HOLESKY_BLOCK_2_498_456: &[u8; 235_397] =
+    include_bytes!("./headers/holesky-block-2_498_456.json");
+pub const HOLESKY_HEADER_2_498_457: &[u8; 670] =
+    include_bytes!("./headers/holesky-header-2_498_457.json");
+pub const HOLESKY_HEADER_2_498_458: &[u8; 669] =
+    include_bytes!("./headers/holesky-header-2_498_458.json");
+pub const HOLESKY_HEADER_2_498_459: &[u8; 670] =
+    include_bytes!("./headers/holesky-header-2_498_459.json");
+pub const HOLESKY_HEADER_2_498_460: &[u8; 670] =
+    include_bytes!("./headers/holesky-header-2_498_460.json");
+pub const HOLESKY_HEADER_2_498_461: &[u8; 670] =
+    include_bytes!("./headers/holesky-header-2_498_461.json");
+pub const HOLESKY_HEADER_2_498_462: &[u8; 669] =
+    include_bytes!("./headers/holesky-header-2_498_462.json");
+pub const HOLESKY_HEADER_2_498_463: &[u8; 670] =
+    include_bytes!("./headers/holesky-header-2_498_463.json");
+pub const HOLESKY_HEADER_2_498_464: &[u8; 669] =
+    include_bytes!("./headers/holesky-header-2_498_464.json");
+
+#[derive(Deserialize)]
+pub struct Receipts {
+    result: Vec<TransactionReceipt>,
+}
+
+pub fn event() -> EthToVaraEvent {
+    // tx 0x180cd2328df9c4356adc77e19e33c5aa2d5395f1b52e70d22c25070a04f16691
+    let tx_index = 15;
+
+    let receipts: Receipts = serde_json::from_slice(HOLESKY_RECEIPTS_2_498_456.as_ref()).unwrap();
+    let receipts = receipts
+        .result
+        .iter()
+        .map(|tx_receipt| {
+            let receipt = tx_receipt.as_ref();
+
+            tx_receipt
+                .transaction_index
+                .map(|i| (i, eth_utils::map_receipt_envelope(receipt)))
+        })
+        .collect::<Option<Vec<_>>>()
+        .unwrap_or_default();
+
+    let block: Block = {
+        let response: BeaconBlockResponse<Block> =
+            serde_json::from_slice(HOLESKY_BLOCK_2_498_456.as_ref()).unwrap();
+
+        response.data.message.into()
+    };
+    let headers = vec![
+        {
+            let response: BeaconBlockHeaderResponse =
+                serde_json::from_slice(HOLESKY_HEADER_2_498_457.as_ref()).unwrap();
+
+            response.data.header.message
+        },
+        {
+            let response: BeaconBlockHeaderResponse =
+                serde_json::from_slice(HOLESKY_HEADER_2_498_458.as_ref()).unwrap();
+
+            response.data.header.message
+        },
+        {
+            let response: BeaconBlockHeaderResponse =
+                serde_json::from_slice(HOLESKY_HEADER_2_498_459.as_ref()).unwrap();
+
+            response.data.header.message
+        },
+        {
+            let response: BeaconBlockHeaderResponse =
+                serde_json::from_slice(HOLESKY_HEADER_2_498_460.as_ref()).unwrap();
+
+            response.data.header.message
+        },
+        {
+            let response: BeaconBlockHeaderResponse =
+                serde_json::from_slice(HOLESKY_HEADER_2_498_461.as_ref()).unwrap();
+
+            response.data.header.message
+        },
+        {
+            let response: BeaconBlockHeaderResponse =
+                serde_json::from_slice(HOLESKY_HEADER_2_498_462.as_ref()).unwrap();
+
+            response.data.header.message
+        },
+        {
+            let response: BeaconBlockHeaderResponse =
+                serde_json::from_slice(HOLESKY_HEADER_2_498_463.as_ref()).unwrap();
+
+            response.data.header.message
+        },
+        {
+            let response: BeaconBlockHeaderResponse =
+                serde_json::from_slice(HOLESKY_HEADER_2_498_464.as_ref()).unwrap();
+
+            response.data.header.message
+        },
+    ];
+
+    let MerkleProof { proof, receipt } =
+        eth_utils::generate_merkle_proof(tx_index, &receipts[..]).unwrap();
+
+    let mut receipt_rlp = Vec::with_capacity(Encodable::length(&receipt));
+    Encodable::encode(&receipt, &mut receipt_rlp);
+
+    let block = BlockGenericForBlockBody {
+        slot: block.slot,
+        proposer_index: block.proposer_index,
+        parent_root: block.parent_root,
+        state_root: block.state_root,
+        body: block.body.into(),
+    };
+
+    EthToVaraEvent {
+        proof_block: BlockInclusionProof {
+            block,
+            headers,
+        },
+        proof: proof.clone(),
+        transaction_index: tx_index,
+        receipt_rlp,
+    }
+}

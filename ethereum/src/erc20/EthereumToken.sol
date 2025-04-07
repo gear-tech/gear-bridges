81f6a4b104ec227c01e4f30f486cb9ff15246480
mertwole <mertwole@gmail.com>
2025-04-07 10:52:15 +0000

    Add doc-comments

--- ethereum/src/erc20/EthereumToken.sol
+++ ethereum/src/erc20/EthereumToken.sol
@@ -12,10 +12,20 @@ contract EthereumToken is ERC20 {
         return 18;
     }
 
+    /** @dev Convert native Ethereum into the `EthereumToken` ERC-20 token.
+     *
+     *  The conversion will be done in 1:1 ratio.
+     */
     function tokenize() public payable {
         _mint(msg.sender, msg.value);
     }
 
+    /** @dev Convert `EthereumToken` ERC-20 token into native Ethereum.
+     *
+     *  The conversion will be done in 1:1 ratio.
+     *
+     *  @param amount - Amount of the `EthereumToken` to be exchanged to a native token.
+     */
     function release(uint256 amount) public {
         _burn(msg.sender, amount);
         payable(msg.sender).transfer(amount);

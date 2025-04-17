9989e75409091e36c5050793e0725ab8627b3680
Nikita Yutanov <nikitayutanov@gmail.com>
2025-04-17 16:07:26 +0300

    add search params to owner and status fields

--- frontend/src/features/history/hooks/use-transaction-filters.ts
+++ frontend/src/features/history/hooks/use-transaction-filters.ts
@@ -3,6 +3,7 @@ import { useAccount } from '@gear-js/react-hooks';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMemo } from 'react';
 import { useForm } from 'react-hook-form';
+import { useSearchParams } from 'react-router-dom';
 import { z } from 'zod';
 
 import { useDebounce, useEthAccount } from '@/hooks';
@@ -31,8 +32,34 @@ function useTransactionFilters() {
   const ethAccount = useEthAccount();
   const accountAddress = account?.decodedAddress || ethAccount.address;
 
+  const [searchParams] = useSearchParams();
+
+  const getOwnerDefaultValue = () => {
+    if (!accountAddress) return DEFAULT_VALUES[FIELD_NAME.OWNER];
+
+    const owner = searchParams.get('owner');
+
+    if (owner === 'true') return true;
+    if (owner === 'false') return false;
+
+    return DEFAULT_VALUES[FIELD_NAME.OWNER];
+  };
+
+  const getStatusDefaultValue = () => {
+    const status = searchParams.get('status') as Status;
+
+    if (Object.values(Status).includes(status)) return status;
+
+    return DEFAULT_VALUES[FIELD_NAME.STATUS];
+  };
+
   const form = useForm({
-    defaultValues: DEFAULT_VALUES,
+    defaultValues: {
+      // TODO: search params for each field, field should change param on change
+      ...DEFAULT_VALUES,
+      [FIELD_NAME.OWNER]: getOwnerDefaultValue(),
+      [FIELD_NAME.STATUS]: getStatusDefaultValue(),
+    },
     mode: 'onChange',
     resolver: zodResolver(SCHEMA),
   });

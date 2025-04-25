f063b6f84a24e34197dcef5ee0940057806881b1
Nikita Yutanov <nikitayutanov@gmail.com>
2025-04-26 00:40:02 +0300

    remove event listener from eth approve

--- frontend/src/features/swap/hooks/eth/use-approve.ts
+++ frontend/src/features/swap/hooks/eth/use-approve.ts
@@ -1,50 +1,23 @@
 import { useMutation } from '@tanstack/react-query';
-import { encodeFunctionData, WatchContractEventOnLogsParameter } from 'viem';
+import { encodeFunctionData } from 'viem';
 import { useConfig, useWriteContract } from 'wagmi';
-import { estimateGas, watchContractEvent } from 'wagmi/actions';
+import { estimateGas, waitForTransactionReceipt } from 'wagmi/actions';
 
 import { FUNGIBLE_TOKEN_ABI } from '@/consts';
-import { useEthAccount } from '@/hooks';
 import { definedAssert } from '@/utils';
 
-import { ERC20_MANAGER_CONTRACT_ADDRESS, EVENT_NAME } from '../../consts';
+import { ERC20_MANAGER_CONTRACT_ADDRESS } from '../../consts';
 import { FUNCTION_NAME } from '../../consts/eth';
 import { useBridgeContext } from '../../context';
 
 const abi = FUNGIBLE_TOKEN_ABI;
 
 function useApprove() {
-  const ethAccount = useEthAccount();
-
   const { token } = useBridgeContext();
   const { address } = token;
 
   const config = useConfig();
   const { writeContractAsync } = useWriteContract();
-
-  // maybe better to use waitForTransactionReceipt,
-  // but feels like it's getting fired before approval in contract
-  const watch = (amount: bigint) =>
-    new Promise<bigint>((resolve, reject) => {
-      const eventName = EVENT_NAME.APPROVAL;
-      const args = { owner: ethAccount.address, spender: ERC20_MANAGER_CONTRACT_ADDRESS };
-
-      const onLogs = (logs: WatchContractEventOnLogsParameter<typeof abi, typeof EVENT_NAME.APPROVAL>) =>
-        logs.forEach(({ args: { value = 0n } }) => {
-          unwatch();
-
-          if (value < amount) return reject(new Error('Approved value is less than the required amount'));
-
-          resolve(value);
-        });
-
-      const onError = (error: Error) => {
-        unwatch();
-        reject(error);
-      };
-
-      const unwatch = watchContractEvent(config, { address, abi, eventName, args, onLogs, onError });
-    });
 
   const getGasLimit = (amount: bigint) => {
     definedAssert(address, 'Fungible token address');
@@ -63,7 +36,9 @@ function useApprove() {
     const functionName = FUNCTION_NAME.FUNGIBLE_TOKEN_APPROVE;
     const args = [ERC20_MANAGER_CONTRACT_ADDRESS, amount] as const;
 
-    return writeContractAsync({ address, abi, functionName, args, gas }).then(() => watch(amount));
+    const hash = await writeContractAsync({ address, abi, functionName, args, gas });
+
+    return waitForTransactionReceipt(config, { hash });
   };
 
   return { ...useMutation({ mutationFn: approve }), getGasLimit };

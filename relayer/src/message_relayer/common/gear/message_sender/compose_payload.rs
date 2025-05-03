9cfbb6be0ed8a6dc247922690e6fc2ff96b8cf8b
playX18 <158266309+playX18@users.noreply.github.com>
2025-05-03 18:21:26 +0700

    refactor(relayer): use historical-proxy and verify headers in compose_payload (#362)
    Co-authored-by: Georgy Shepelev <geo.shepelev@gmail.com>

--- relayer/src/message_relayer/common/gear/message_sender/compose_payload.rs
+++ relayer/src/message_relayer/common/gear/message_sender/compose_payload.rs
@@ -1,23 +1,31 @@
+use std::ops::ControlFlow;
+
+use checkpoint_light_client_client::{traits::ServiceCheckpointFor as _, ServiceCheckpointFor};
 use ethereum_beacon_client::BeaconClient;
 
 use alloy::{network::primitives::BlockTransactionsKind, primitives::TxHash, providers::Provider};
 use alloy_eips::{BlockId, BlockNumberOrTag};
 use alloy_rlp::Encodable;
 use anyhow::{anyhow, Result as AnyResult};
-use sails_rs::prelude::*;
+use ethereum_common::tree_hash::TreeHash;
+use historical_proxy_client::{traits::HistoricalProxy as _, HistoricalProxy};
+use sails_rs::{calls::Query, gclient::calls::GClientRemoting, prelude::*};
 
 use checkpoint_light_client_io::ethereum_common::{
     beacon,
     utils::{self as eth_utils, MerkleProof},
-    SLOTS_PER_EPOCH,
 };
-use eth_events_electra_client::{BlockGenericForBlockBody, BlockInclusionProof, EthToVaraEvent};
+use eth_events_electra_client::{
+    traits::EthereumEventClient, BlockGenericForBlockBody, BlockInclusionProof, EthToVaraEvent,
+};
 use ethereum_client::EthApi;
 
 pub async fn compose(
     beacon_client: &BeaconClient,
+    gear_api: &gclient::GearApi,
     eth_client: &EthApi,
     tx_hash: TxHash,
+    historical_proxy_id: ActorId,
 ) -> AnyResult<EthToVaraEvent> {
     let provider = eth_client.raw_provider();
 
@@ -46,8 +54,14 @@ pub async fn compose(
         .ok_or(anyhow!("Unable to determine root of parent beacon block"))?;
     let block_number = block.header.number;
 
-    let proof_block =
-        build_inclusion_proof(beacon_client, &beacon_root_parent, block_number).await?;
+    let proof_block = build_inclusion_proof(
+        beacon_client,
+        gear_api,
+        &beacon_root_parent,
+        block_number,
+        historical_proxy_id,
+    )
+    .await?;
 
     // receipt Merkle-proof
     let tx_index = receipt
@@ -83,9 +97,17 @@ pub async fn compose(
 
 async fn build_inclusion_proof(
     beacon_client: &BeaconClient,
+    gear_api: &gclient::GearApi,
     beacon_root_parent: &[u8; 32],
     block_number: u64,
+    historical_proxy_id: ActorId,
 ) -> AnyResult<BlockInclusionProof> {
+    let remoting = GClientRemoting::new(gear_api.clone());
+
+    let historical_proxy = HistoricalProxy::new(remoting.clone());
+    let eth_events = eth_events_electra_client::EthereumEventClient::new(remoting.clone());
+    let service_checkpoint = ServiceCheckpointFor::new(remoting);
+
     let beacon_block_parent = beacon_client
         .get_block_by_hash::<beacon::electra::Block>(beacon_root_parent)
         .await?;
@@ -98,6 +120,27 @@ async fn build_inclusion_proof(
         .await?;
 
     let slot = beacon_block.slot;
+
+    let endpoint = historical_proxy
+        .endpoint_for(slot)
+        .recv(historical_proxy_id)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?
+        .map_err(|e| anyhow::anyhow!("Proxy faield to get endpoint for slot #{}: {:?}", slot, e))?;
+
+    let checkpoint_endpoint = eth_events
+        .checkpoint_light_client_address()
+        .recv(endpoint)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?;
+
+    let (checkpoint_slot, checkpoint) = service_checkpoint
+        .get(slot)
+        .recv(checkpoint_endpoint)
+        .await
+        .map_err(|e| anyhow::anyhow!(e))?
+        .map_err(|e| anyhow::anyhow!("Checkpoint error: {:?}", e))?;
+
     let block = BlockGenericForBlockBody {
         slot,
         proposer_index: beacon_block.proposer_index,
@@ -105,22 +148,36 @@ async fn build_inclusion_proof(
         state_root: beacon_block.state_root,
         body: beacon_block.body.into(),
     };
-    if slot % SLOTS_PER_EPOCH == 0 {
+    if slot == checkpoint_slot {
         return Ok(BlockInclusionProof {
             block,
             headers: vec![],
         });
     }
 
-    let epoch_next = 1 + eth_utils::calculate_epoch(slot);
-    let slot_checkpoint = epoch_next * SLOTS_PER_EPOCH;
+    let headers = {
+        let mut headers = beacon_client
+            .request_headers(slot + 1, checkpoint_slot + 1)
+            .await?;
 
-    Ok(BlockInclusionProof {
-        block,
-        headers: beacon_client
-            .request_headers(slot + 1, slot_checkpoint + 1)
-            .await?
-            .into_iter()
-            .collect(),
-    })
+        headers.sort_unstable_by(|a, b| a.slot.cmp(&b.slot));
+
+        headers
+    };
+    let ControlFlow::Continue(_) =
+        headers
+            .iter()
+            .rev()
+            .try_fold(checkpoint, |block_root_parent, header| {
+                let block_root = header.tree_hash_root();
+                match block_root == block_root_parent {
+                    true => ControlFlow::Continue(header.parent_root),
+                    false => ControlFlow::Break(()),
+                }
+            })
+    else {
+        return Err(anyhow::anyhow!("Invalid block proof"));
+    };
+
+    Ok(BlockInclusionProof { block, headers })
 }

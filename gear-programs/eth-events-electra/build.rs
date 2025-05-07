b7312b6d69e7a3933978fe4286372485d6712044
playX18 <adel.prokurov281@outlook.com>
2025-05-07 14:34:37 +0700

    fmt

--- gear-programs/eth-events-electra/build.rs
+++ gear-programs/eth-events-electra/build.rs
@@ -19,8 +19,5 @@ fn main() {
 
     let mut idl_path = PathBuf::from(bin_path);
     idl_path.set_extension("idl");
-    sails_idl_gen::generate_idl_to_file::<eth_events_electra_app::Program>(
-        idl_path,
-    )
-    .unwrap();
+    sails_idl_gen::generate_idl_to_file::<eth_events_electra_app::Program>(idl_path).unwrap();
 }

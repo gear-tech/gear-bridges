ae86fb257c7135bf14868db036fdbc8573461a9e
playX18 <adel.prokurov281@outlook.com>
2025-05-23 04:39:29 +0000

    connect_node increments salt only once, use LazyLock for state

--- tests/src/lib.rs
+++ tests/src/lib.rs
@@ -1,20 +1,28 @@
 use std::{collections::hash_map::Entry, sync::LazyLock};
 
-use alloy::primitives::map::{HashMap, HashSet};
 use gclient::{GearApi, WSAddress};
 use gear_core::ids::prelude::*;
 use sails_rs::{calls::*, prelude::*};
-use tokio::sync::Mutex;
 use sp_core::crypto::DEV_PHRASE;
+use tokio::sync::Mutex;
 #[cfg(test)]
 mod historical_proxy;
 
-static LOCK: LazyLock<Mutex<(u32, std::collections::HashMap<&'static [u8], CodeId, std::hash::RandomState>)>> =
-    LazyLock::new(|| Mutex::const_new((2_000, std::collections::HashMap::new())));
+static LOCK: LazyLock<
+    Mutex<(
+        u32,
+        std::collections::HashMap<&'static [u8], CodeId, std::hash::RandomState>,
+    )>,
+> = LazyLock::new(|| Mutex::const_new((4_0000_2_000, std::collections::HashMap::new())));
+
+
+pub const DEFAULT_BALANCE: u128 = 500_000_000_000_000;
 
 pub struct Connection {
-    accounts: Vec<(GearApi, ActorId, [u8; 4])>,
-    code_ids: Vec<CodeId>,
+    pub accounts: Vec<(GearApi, ActorId)>,
+    pub code_ids: Vec<CodeId>,
+    pub salt: [u8; 4],
+    pub gas_limit: u64,
 }
 
 pub async fn connect_to_node(
@@ -25,17 +33,19 @@ pub async fn connect_to_node(
     let mut lock = LOCK.lock().await;
 
     let api = GearApi::dev().await.unwrap();
-
+    let gas_limit = api.block_gas_limit().unwrap();
     let code_ids = {
         let mut res = vec![];
 
         for &binary in binaries {
             match lock.1.entry(binary) {
                 Entry::Occupied(entry) => {
+                    println!("code {:p} already uploaded", binary);
                     res.push(*entry.get());
                 }
 
                 Entry::Vacant(entry) => {
+                    println!("uploading code {:p}", binary);
                     let code_id = api
                         .upload_code(binary)
                         .await
@@ -49,23 +59,26 @@ pub async fn connect_to_node(
 
         res
     };
-
+    let salt = lock.0;
+    lock.0 += 1;
     let mut accounts = vec![];
-    for (i, &balance) in balances.iter().enumerate() {
-        let salt = lock.0;
-        lock.0 += 1;
-
-        let suri = format!("{DEV_PHRASE}//{program}-{salt}");
-        let api2 = GearApi::init_with(WSAddress::dev(), suri).await.unwrap();
+    for &balance in balances.iter() {
+        let suri = format!("{DEV_PHRASE}//{program}-{salt}:");
+        let api2 = GearApi::init_with(WSAddress::dev(), suri.clone()).await.unwrap();
 
         let account_id: &[u8; 32] = api2.account_id().as_ref();
-
+        println!("account {} with SURI={} and balance={}", api2.account_id(), suri, balance);
         api.transfer_keep_alive((*account_id).into(), balance)
             .await
             .unwrap();
         let account_id = ActorId::from(*account_id);
-        accounts.push((api2, account_id, salt.to_le_bytes()));
+        accounts.push((api2, account_id));
     }
 
-    Connection { accounts, code_ids }
+    Connection {
+        accounts,
+        code_ids,
+        salt: salt.to_le_bytes(),
+        gas_limit,
+    }
 }

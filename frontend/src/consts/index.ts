c3ab7b3c38cdfbe7100ec877d182151a16915524
Nikita Yutanov <nikitayutanov@gmail.com>
2025-06-15 13:37:55 +0300

    migrate to wrapped eth

--- frontend/src/consts/index.ts
+++ frontend/src/consts/index.ts
@@ -1,11 +1,11 @@
-import { FUNGIBLE_TOKEN_ABI, ETH_TOKEN_ABI } from './abi';
+import { FUNGIBLE_TOKEN_ABI, WRAPPED_ETH_ABI } from './abi';
 import { VARA_NODE_ADDRESS, ETH_NODE_ADDRESS, ETH_CHAIN_ID, VFT_MANAGER_CONTRACT_ADDRESS, GTM_ID } from './env';
 import { ROUTE } from './routing';
 import { VftManagerProgram, VftProgram, WrappedVaraProgram } from './sails';
 
 export {
   FUNGIBLE_TOKEN_ABI,
-  ETH_TOKEN_ABI,
+  WRAPPED_ETH_ABI,
   VARA_NODE_ADDRESS,
   ETH_NODE_ADDRESS,
   ETH_CHAIN_ID,

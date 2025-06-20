ed9c24cdb1288fb42a6431c7164c35fdcf1c259e
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2025-06-20 16:21:08 +0400

    feat(ethereum, gear-programs): add sender address to payload  (#468)

--- ethereum/src/interfaces/IERC20Manager.sol
+++ ethereum/src/interfaces/IERC20Manager.sol
@@ -9,7 +9,7 @@ interface IERC20Manager {
 
     event BridgingRequested(address indexed from, bytes32 indexed to, address indexed token, uint256 amount);
 
-    event BridgingAccepted(address indexed to, address indexed token, uint256 amount);
+    event BridgingAccepted(address indexed to, address indexed token, uint256 amount, bytes32 tokens_sender);
 
     enum SupplyType {
         Unknown,
@@ -22,10 +22,11 @@ struct WithdrawMessage {
     address receiver;
     address token;
     uint256 amount;
+    bytes32 tokens_sender;
 }
 
 library Packer {
     function pack(WithdrawMessage calldata message) external pure returns (bytes memory) {
-        return abi.encodePacked(message.receiver, message.token, message.amount);
+        return abi.encodePacked(message.receiver, message.token, message.amount, message.tokens_sender);
     }
 }

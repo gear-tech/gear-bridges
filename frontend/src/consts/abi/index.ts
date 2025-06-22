d7e27243ffd9b75fd0131962adb40d5cc671e548
Nikita Yutanov <nikitayutanov@gmail.com>
2025-06-22 12:43:57 +0300

    rename erc20 abi

--- frontend/src/consts/abi/index.ts
+++ frontend/src/consts/abi/index.ts
@@ -1,4 +1,4 @@
-import { FUNGIBLE_TOKEN_ABI } from './fungible-token-abi';
+import { ERC20_ABI } from './erc20-abi';
 import { WRAPPED_ETH_ABI } from './wrapped-eth';
 
-export { FUNGIBLE_TOKEN_ABI, WRAPPED_ETH_ABI };
+export { ERC20_ABI, WRAPPED_ETH_ABI };

b53fb171b19535076cc64a0e1aa157ef8ecb3bc8
Nikita Yutanov <nikitayutanov@gmail.com>
2025-07-28 15:35:39 +0400

    feat(frontend): add update warning card

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,3 +1,4 @@
+import WarningSVG from '@/assets/warning.svg?react';
 import { Container } from '@/components';
 import { PendingTransactionsWarning } from '@/features/history';
 import { Swap } from '@/features/swap';
@@ -7,6 +8,28 @@ import styles from './home.module.scss';
 function Home() {
   return (
     <Container maxWidth="640px" className={styles.container}>
+      <div className={styles.warning}>
+        <WarningSVG className={styles.icon} />
+
+        <div className={styles.text}>
+          <p>Testnet Update on 31-Aug-2025.</p>
+
+          <p>
+            A major Bridge upgrade is scheduled, which includes important fixes, stability improvements, and a
+            transition of the Ethereum testnet environment from Holesky to Hoodi.
+          </p>
+
+          <p>Please note:</p>
+
+          <ul>
+            <li>• Previous test transaction history will be removed from the UI (real funds are not affected).</li>
+            <li>• New transactions will appear as usual.</li>
+          </ul>
+
+          <p>Thanks for supporting the test phase!</p>
+        </div>
+      </div>
+
       <PendingTransactionsWarning />
       <Swap />
     </Container>

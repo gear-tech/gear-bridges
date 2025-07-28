cd140e1bcbaa8a43a427698be953bd576814805f
Nikita Yutanov <nikitayutanov@gmail.com>
2025-07-28 18:14:12 +0400

    fix(frontend): update warn styles

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -12,20 +12,15 @@ function Home() {
         <WarningSVG className={styles.icon} />
 
         <div className={styles.text}>
-          <p>Testnet Update on 31-Aug-2025.</p>
-
+          <p className={styles.heading}>Testnet Update on 31-Jul-2025.</p>
           <p>
             A major Bridge upgrade is scheduled, which includes important fixes, stability improvements, and a
             transition of the Ethereum testnet environment from Holesky to Hoodi.
           </p>
-
-          <p>Please note:</p>
-
-          <ul>
-            <li>• Previous test transaction history will be removed from the UI (real funds are not affected).</li>
-            <li>• New transactions will appear as usual.</li>
-          </ul>
-
+          <p>
+            Please note - previous test transaction history will be removed from the UI (real funds are not affected).
+            New transactions will appear as usual.
+          </p>
           <p>Thanks for supporting the test phase!</p>
         </div>
       </div>

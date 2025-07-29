d124ab97092a293ef403e17e68cf4110432fdc5c
Dmitri Osipov <dmith.osipov@gmail.com>
2025-07-29 17:37:51 +0400

    fix(indexer): build docker, migrations (#546)

--- indexer/src/model/dataSource.ts
+++ indexer/src/model/dataSource.ts
@@ -14,11 +14,7 @@ dotenv.config();
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '5432'),
-  username: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || 'postgres',
-  database: process.env.DB_NAME || 'gear_monitoring',
+  url: process.env.DATABASE_URL,
   synchronize: false,
   migrationsRun: true,
   logging: process.env.NODE_ENV === 'development',

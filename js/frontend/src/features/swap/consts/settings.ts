5de37f9e8057d9b47930f579f17051c95676237b
Nikita Yutanov <nikitayutanov@gmail.com>
2025-08-25 18:00:05 +0400

    divide state and component logic

---
+++ js/frontend/src/features/swap/consts/settings.ts
@@ -0,0 +1,11 @@
+const PRIORITY = {
+  DEFAULT: 'default',
+  HIGH: 'high',
+} as const;
+
+const CLAIM_TYPE = {
+  MANUAL: 'manual',
+  AUTO: 'auto',
+} as const;
+
+export { PRIORITY, CLAIM_TYPE };

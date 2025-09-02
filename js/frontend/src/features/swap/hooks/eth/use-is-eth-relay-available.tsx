23b1730f226dff3ed6ad84c5caca84d3e97d147c
Nikita Yutanov <nikitayutanov@gmail.com>
2025-09-02 20:40:53 +0400

    add alert on `getSlotByBlockNumber` error

--- js/frontend/src/features/swap/hooks/eth/use-is-eth-relay-available.tsx
+++ js/frontend/src/features/swap/hooks/eth/use-is-eth-relay-available.tsx
@@ -1,6 +1,8 @@
 import { getSlotByBlockNumber } from '@gear-js/bridge';
+import { useAlert } from '@gear-js/react-hooks';
 import { useQuery } from '@tanstack/react-query';
 import { request } from 'graphql-request';
+import { useEffect } from 'react';
 import { usePublicClient } from 'wagmi';
 
 import { INDEXER_ADDRESS } from '@/features/history/consts';
@@ -17,14 +19,22 @@ const CHECKPOINT_SLOTS_QUERY = graphql(`
 `);
 
 function useIsEthRelayAvailable(blockNumber: bigint) {
+  const alert = useAlert();
   const publicClient = usePublicClient();
 
-  const { data: slot } = useQuery({
+  const { data: slot, error } = useQuery({
     queryKey: ['slotByBlockNumber', blockNumber.toString()],
     queryFn: () => getSlotByBlockNumber(ETH_BEACON_NODE_ADDRESS, publicClient!, blockNumber),
     enabled: Boolean(publicClient),
     select: (data) => data?.toString(),
   });
+
+  useEffect(() => {
+    if (!error) return;
+
+    alert.error(error.message);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [error]);
 
   return useQuery({
     queryKey: ['isEthRelayAvailable', slot],

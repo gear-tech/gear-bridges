4ac09f55661f0df37c091fdc7a86a88e78c4915a
Nikita Yutanov <nikitayutanov@gmail.com>
2025-09-26 16:19:22 +0400

    add optimistic tx update on paid fee

--- js/frontend/src/features/history/hooks/index.ts
+++ js/frontend/src/features/history/hooks/index.ts
@@ -1,8 +1,16 @@
 import { usePairs } from './use-pairs';
 import { usePendingTxsCount } from './use-pending-txs-count';
-import { useTransaction } from './use-transaction';
+import { useTransaction, useOptimisticTxUpdate } from './use-transaction';
 import { useTransactionFilters } from './use-transaction-filters';
 import { useTransactions } from './use-transactions';
 import { useTransactionsCount } from './use-transactions-count';
 
-export { usePairs, useTransactionsCount, useTransactions, useTransactionFilters, useTransaction, usePendingTxsCount };
+export {
+  usePairs,
+  useTransactionsCount,
+  useTransactions,
+  useTransactionFilters,
+  useTransaction,
+  useOptimisticTxUpdate,
+  usePendingTxsCount,
+};

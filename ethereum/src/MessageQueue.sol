43642198fd9df38bc11f048e0af067c017bdf103
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2025-09-28 12:11:44 +0500

    feat(ethereum): add `script/upgrades/*` to deploy new implementations (#627)

--- ethereum/src/MessageQueue.sol
+++ ethereum/src/MessageQueue.sol
@@ -102,6 +102,11 @@ contract MessageQueue is
     }
 
     /**
+     * @custom:oz-upgrades-validate-as-initializer
+     */
+    // function reinitialize() public onlyRole(DEFAULT_ADMIN_ROLE) reinitializer(2) {}
+
+    /**
      * @dev Returns governance admin address.
      * @return governanceAdmin Governance admin address.
      */

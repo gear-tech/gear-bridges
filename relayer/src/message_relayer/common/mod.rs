4d24715250ab1ad99a21de30580fd6443be6c3c6
Adel Prokurov <adel.prokurov281@outlook.com>
2025-09-29 19:48:11 +0500

    fix(relayer): change message nonces from LE to BE

--- relayer/src/message_relayer/common/mod.rs
+++ relayer/src/message_relayer/common/mod.rs
@@ -175,11 +175,10 @@ fn message_queued_events_of(
 ) -> impl Iterator<Item = gear_rpc_client::dto::Message> + use<'_> {
     block.events().iter().filter_map(|event| match event {
         gclient::Event::GearEthBridge(GearEthBridgeEvent::MessageQueued { message, .. }) => {
-            let mut nonce_le = [0; 32];
-            primitive_types::U256(message.nonce.0).to_little_endian(&mut nonce_le);
-
+            let mut nonce_be = [0; 32];
+            primitive_types::U256(message.nonce.0).to_big_endian(&mut nonce_be);
             Some(gear_rpc_client::dto::Message {
-                nonce_le,
+                nonce_be,
                 source: message.source.0,
                 destination: message.destination.0,
                 payload: message.payload.clone(),
@@ -191,7 +190,7 @@ fn message_queued_events_of(
 
 pub fn message_hash(message: &Message) -> [u8; 32] {
     let data = [
-        message.nonce_le.as_ref(),
+        message.nonce_be.as_ref(),
         message.source.as_ref(),
         message.destination.as_ref(),
         message.payload.as_ref(),

8a66018959dd9b7abc10d0ec29fae69c168b7fa4
Dmitri Osipov <dmith.osipov@gmail.com>
2025-10-15 13:53:28 +0200

    fix(js/indexer): saving merkle roots submitted to MessageQueue contract

--- js/indexer/src/eth/batch-state.ts
+++ js/indexer/src/eth/batch-state.ts
@@ -2,16 +2,17 @@ import { DataHandlerContext } from '@subsquid/evm-processor';
 import { Store } from '@subsquid/typeorm-store';
 
 import { MerkleRootInMessageQueue, Network, Status } from '../model/index.js';
-import { BaseBatchState, setValues } from '../common/index.js';
+import { BaseBatchState, mapKeys, mapValues } from '../common/index.js';
+import { In } from 'typeorm';
 
 export class BatchState extends BaseBatchState<DataHandlerContext<Store, any>> {
   private _paidRequests: Set<string>;
-  private _merkleRoots: Set<MerkleRootInMessageQueue>;
+  private _merkleRoots: Map<string, MerkleRootInMessageQueue>;
 
   constructor() {
     super(Network.Ethereum, Network.Vara);
     this._paidRequests = new Set();
-    this._merkleRoots = new Set();
+    this._merkleRoots = new Map();
   }
 
   protected _clear(): void {
@@ -41,7 +42,29 @@ export class BatchState extends BaseBatchState<DataHandlerContext<Store, any>> {
   private async _saveMerkleRoots() {
     if (this._merkleRoots.size === 0) return;
 
-    await this._ctx.store.save(setValues(this._merkleRoots));
+    const savedBlocks = await this._ctx.store.findBy(MerkleRootInMessageQueue, {
+      blockNumber: In(mapKeys(this._merkleRoots)),
+    });
+
+    const values = mapValues(this._merkleRoots);
+    const savedBlockNumbers: bigint[] = [];
+
+    if (savedBlocks.length > 0) {
+      this._log.warn(
+        {
+          saved: savedBlocks.map(({ blockNumber, merkleRoot }) => ({ bn: blockNumber, mr: merkleRoot })),
+          duplicates: values.map(({ blockNumber, merkleRoot }) => ({
+            bn: blockNumber,
+            mr: merkleRoot,
+          })),
+        },
+        `Merkle root duplicates found`,
+      );
+
+      savedBlockNumbers.push(...savedBlocks.map(({ blockNumber }) => blockNumber));
+    }
+
+    await this._ctx.store.save(values.filter(({ blockNumber }) => savedBlockNumbers.includes(blockNumber)));
 
     this._log.info({ count: this._merkleRoots.size }, 'Merkle roots saved');
   }
@@ -53,6 +76,6 @@ export class BatchState extends BaseBatchState<DataHandlerContext<Store, any>> {
 
   public newMerkleRoot(blockNumber: bigint, merkleRoot: string) {
     this._log.info({ blockNumber, merkleRoot }, 'Merkle root received');
-    this._merkleRoots.add(new MerkleRootInMessageQueue({ blockNumber, merkleRoot }));
+    this._merkleRoots.set(blockNumber.toString(), new MerkleRootInMessageQueue({ blockNumber, merkleRoot }));
   }
 }

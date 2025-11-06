d47952d849f3c0a1e8fe9dcae2cdbd6f55b4371b
Nikita Yutanov <nikitayutanov@gmail.com>
2025-11-07 01:51:49 +0400

    fix default wallet network

--- js/frontend/src/providers.tsx
+++ js/frontend/src/providers.tsx
@@ -75,7 +75,21 @@ function setDefaultWagmiChain() {
   }
 }
 
+function setDefaultAppkitChain() {
+  const id = localStorage.getItem('@appkit/active_caip_network_id');
+
+  if (id) {
+    const splitted = id.split(':');
+
+    splitted[1] =
+      NETWORK_PRESET[DEFAULT_NETWORK_TYPE.toUpperCase() as keyof typeof NETWORK_PRESET].ETH_CHAIN_ID.toString();
+
+    localStorage.setItem('@appkit/active_caip_network_id', splitted.join(':'));
+  }
+}
+
 setDefaultWagmiChain();
+setDefaultAppkitChain();
 
 const adapter = new WagmiAdapter({
   networks,

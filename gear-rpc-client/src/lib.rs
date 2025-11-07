35c9666c86acd029cb2910ce8a1bd38d3ec9039e
Adel Prokurov <adel.prokurov281@outlook.com>
2025-11-07 11:19:18 +0700

    fmt

--- gear-rpc-client/src/lib.rs
+++ gear-rpc-client/src/lib.rs
@@ -226,7 +226,7 @@ impl GearApi {
             let hex_string = res?;
             let bytes = hex::decode(&hex_string[2..]).context("failed to decoded hex")?;
             let justification = GrandpaJustification::<GearHeader>::decode(&mut &bytes[..])?;
-            
+
             Ok(justification)
         });
 

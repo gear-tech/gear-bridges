08091c58a652221a26ac72c8bbc730a76cb0be89
Nikita Yutanov <nikitayutanov@gmail.com>
2025-11-24 23:23:25 +0400

    fix tokens card display

--- js/frontend/src/features/token-tracker/components/tokens-card/tokens-card.tsx
+++ js/frontend/src/features/token-tracker/components/tokens-card/tokens-card.tsx
@@ -1,5 +1,7 @@
+import { HexString } from '@gear-js/api';
 import { useAccount, useAlert } from '@gear-js/react-hooks';
 import { Button } from '@gear-js/vara-ui';
+import { ReactNode } from 'react';
 
 import EthSVG from '@/assets/eth.svg?react';
 import VaraSVG from '@/assets/vara.svg?react';
@@ -7,10 +9,11 @@ import { useTokens } from '@/context';
 import { GetBalanceButton } from '@/features/faucet';
 import { NETWORK } from '@/features/swap/consts';
 import {
-  useVaraAccountBalance,
+  useEthAccount,
   useEthAccountBalance,
-  useVaraFTBalances,
   useEthFTBalances,
+  useVaraAccountBalance,
+  useVaraFTBalances,
   useVaraSymbol,
 } from '@/hooks';
 import { getErrorMessage, isUndefined } from '@/utils';
@@ -20,37 +23,36 @@ import { BalanceCard } from '../balance-card';
 
 import styles from './tokens-card.module.scss';
 
-function TokensCard() {
-  const { account } = useAccount();
-  const isVaraNetwork = Boolean(account);
-  const network = isVaraNetwork ? NETWORK.VARA : NETWORK.ETH;
+type TokensCardProps = {
+  network: { name: string; symbol: string | undefined; id: 'vara' | 'eth'; icon: ReactNode; decimals: number };
+  useBurn: () => { isPending: boolean; mutateAsync: (value: bigint) => Promise<unknown> };
+  useFTBalances: () => { data: Record<string, bigint> | undefined; refetch: () => Promise<unknown> };
+  useAccountBalance: () => { data: bigint | undefined };
+  renderNativeActions: (address: HexString, onSuccess: () => Promise<unknown>) => ReactNode;
+  renderTokenActions?: (address: HexString, symbol: string, onSuccess: () => Promise<unknown>) => ReactNode;
+};
 
-  const varaSymbol = useVaraSymbol();
-  const networkSymbol = isVaraNetwork ? varaSymbol : 'ETH';
+function TokensCardComponent({
+  network,
+  useBurn,
+  useFTBalances,
+  useAccountBalance,
+  renderNativeActions,
+  renderTokenActions,
+}: TokensCardProps) {
   const alert = useAlert();
+  const { tokens, nativeToken } = useTokens();
+  const networkTokens = tokens[network.id];
+  const networkNativeToken = nativeToken[network.id];
 
-  const { tokens, nativeToken: _nativeToken } = useTokens();
-  const networkTokens = tokens[network];
-  const nativeToken = _nativeToken?.[network];
-
-  const burnVara = useBurnVaraTokens();
-  const burnEth = useBurnEthTokens();
-  const burn = account ? burnVara : burnEth;
-
-  const { data: varaFtBalances, refetch: refetchVaraBalances } = useVaraFTBalances();
-  const { data: ethFtBalances, refetch: refetchEthBalances } = useEthFTBalances();
-  const ftBalances = varaFtBalances || ethFtBalances;
-
-  const varaAccountBalance = useVaraAccountBalance();
-  const ethAccountBalance = useEthAccountBalance();
-  const accountBalance = isVaraNetwork ? varaAccountBalance : ethAccountBalance;
+  const burn = useBurn();
+  const { data: ftBalances, refetch: refetchBalances } = useFTBalances();
+  const accountBalance = useAccountBalance();
 
   const handleBurnClick = (value: bigint) => {
-    const sendTx = account ? () => burnVara.sendTransactionAsync({ args: [value] }) : () => burnEth.mutateAsync(value);
-    const refetchBalances = account ? refetchVaraBalances : refetchEthBalances;
-
-    sendTx()
-      .then(async () => {
+    burn
+      .mutateAsync(value)
+      .then(() => {
         alert.success('Tokens converted successfully');
 
         return refetchBalances();
@@ -59,7 +61,7 @@ function TokensCard() {
   };
 
   const renderBalances = () => {
-    if (!networkTokens || !ftBalances || !networkSymbol)
+    if (!networkTokens || !ftBalances || !network.symbol)
       return new Array(5).fill(null).map((_item, index) => <BalanceCard.Skeleton key={index} />);
 
     return networkTokens.map(({ address, decimals, symbol, isNative }) => {
@@ -67,20 +69,18 @@ function TokensCard() {
 
       return (
         <li key={address}>
-          <BalanceCard value={balance} decimals={decimals ?? 0} symbol={symbol ?? 'Unit'} network={network}>
-            {isNative ? (
-              Boolean(balance) && (
-                <Button
-                  text={`Convert To ${networkSymbol}`}
-                  size="small"
-                  onClick={() => handleBurnClick(balance)}
-                  isLoading={burn.isPending}
-                  className={styles.burnButton}
-                />
-              )
-            ) : (
-              <GetBalanceButton.EthToken address={address} symbol={symbol ?? 'Unit'} onSuccess={refetchEthBalances} />
-            )}
+          <BalanceCard value={balance} decimals={decimals ?? 0} symbol={symbol ?? 'Unit'} network={network.id}>
+            {isNative
+              ? Boolean(balance) && (
+                  <Button
+                    text={`Convert To ${network.symbol}`}
+                    size="small"
+                    onClick={() => handleBurnClick(balance)}
+                    isLoading={burn.isPending}
+                    className={styles.burnButton}
+                  />
+                )
+              : renderTokenActions?.(address, symbol ?? 'Unit', refetchBalances)}
           </BalanceCard>
         </li>
       );
@@ -90,27 +90,23 @@ function TokensCard() {
   return (
     <div className={styles.card}>
       <header className={styles.header}>
-        <h2>My Tokens</h2>
+        <h2>Tokens</h2>
 
         <span className={styles.network}>
-          {isVaraNetwork ? <VaraSVG /> : <EthSVG />}
-          {isVaraNetwork ? 'Vara' : 'Ethereum'}
+          {network.icon}
+          {network.name}
         </span>
       </header>
 
       <ul className={styles.list}>
-        {!isUndefined(accountBalance.data) && networkSymbol && nativeToken && (
+        {!isUndefined(accountBalance.data) && network.symbol && networkNativeToken && (
           <li>
             <BalanceCard
               value={accountBalance.data}
-              decimals={isVaraNetwork ? 12 : 18}
-              symbol={networkSymbol}
-              network={network}>
-              {isVaraNetwork ? (
-                <GetBalanceButton.VaraAccount />
-              ) : (
-                <GetBalanceButton.EthToken address={nativeToken.address} symbol="ETH" onSuccess={refetchEthBalances} />
-              )}
+              decimals={network.decimals}
+              symbol={network.symbol}
+              network={network.id}>
+              {renderNativeActions(networkNativeToken.address, refetchBalances)}
             </BalanceCard>
           </li>
         )}
@@ -120,5 +116,48 @@ function TokensCard() {
     </div>
   );
 }
+
+function VaraTokensCard() {
+  const { account } = useAccount();
+  const varaSymbol = useVaraSymbol();
+
+  if (!account) return;
+
+  return (
+    <TokensCardComponent
+      network={{ name: 'Vara', symbol: varaSymbol, id: NETWORK.VARA, icon: <VaraSVG />, decimals: 12 }}
+      useBurn={useBurnVaraTokens}
+      useFTBalances={useVaraFTBalances}
+      useAccountBalance={useVaraAccountBalance}
+      renderNativeActions={() => <GetBalanceButton.VaraAccount />}
+    />
+  );
+}
+
+function EthTokensCard() {
+  const ethAccount = useEthAccount();
+
+  if (!ethAccount.address) return;
+
+  return (
+    <TokensCardComponent
+      network={{ name: 'Ethereum', symbol: 'ETH', id: NETWORK.ETH, icon: <EthSVG />, decimals: 18 }}
+      useBurn={useBurnEthTokens}
+      useFTBalances={useEthFTBalances}
+      useAccountBalance={useEthAccountBalance}
+      renderNativeActions={(address, onSuccess) => (
+        <GetBalanceButton.EthToken address={address} symbol="ETH" onSuccess={onSuccess} />
+      )}
+      renderTokenActions={(address, symbol, onSuccess) => (
+        <GetBalanceButton.EthToken address={address} symbol={symbol} onSuccess={onSuccess} />
+      )}
+    />
+  );
+}
+
+const TokensCard = {
+  Vara: VaraTokensCard,
+  Eth: EthTokensCard,
+};
 
 export { TokensCard };

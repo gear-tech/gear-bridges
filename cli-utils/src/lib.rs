55ee6fe9ede7503c5c7599e27e965105e43d6c95
playX18 <adel.prokurov281@outlook.com>
2026-01-09 09:05:52 +0700

    clippy v2

--- cli-utils/src/lib.rs
+++ cli-utils/src/lib.rs
@@ -21,10 +21,13 @@ mod tests {
 
     #[test]
     fn test_gear_endpoint_parsing() {
-        let cli = TestGearCli::parse_from(&["test", "--gear-endpoint", "wss://example.com"]);
-        assert_eq!(cli.gear.endpoint.as_ref().unwrap().to_string(), "wss://example.com/");
+        let cli = TestGearCli::parse_from(["test", "--gear-endpoint", "wss://example.com"]);
+        assert_eq!(
+            cli.gear.endpoint.as_ref().unwrap().to_string(),
+            "wss://example.com/"
+        );
         assert_eq!(cli.gear.get_endpoint().unwrap(), "wss://example.com/");
-        
+
         let (host, port) = cli.gear.get_host_port().unwrap();
         assert_eq!(host, "wss://example.com");
         assert_eq!(port, 443);
@@ -32,10 +35,16 @@ mod tests {
 
     #[test]
     fn test_gear_domain_port_fallback() {
-        let cli = TestGearCli::parse_from(&["test", "--gear-domain", "ws://localhost", "--gear-port", "9944"]);
+        let cli = TestGearCli::parse_from([
+            "test",
+            "--gear-domain",
+            "ws://localhost",
+            "--gear-port",
+            "9944",
+        ]);
         assert!(cli.gear.endpoint.is_none());
         assert_eq!(cli.gear.get_endpoint().unwrap(), "ws://localhost:9944");
-        
+
         let (host, port) = cli.gear.get_host_port().unwrap();
         assert_eq!(host, "ws://localhost");
         assert_eq!(port, 9944);
@@ -49,7 +58,7 @@ mod tests {
 
     #[test]
     fn test_eth_parsing() {
-        let cli = TestEthCli::parse_from(&["test", "--ethereum-endpoint", "https://eth.com"]);
+        let cli = TestEthCli::parse_from(["test", "--ethereum-endpoint", "https://eth.com"]);
         assert_eq!(cli.eth.endpoint, "https://eth.com");
     }
 }

dfe05f9fa108d4d7eab31f5a5dd5f26aa7fe9529
playX18 <adel.prokurov281@outlook.com>
2026-01-26 14:59:44 +0700

    fix(rpc): use proper call to fetch block number

--- gear-rpc-client/src/lib.rs
+++ gear-rpc-client/src/lib.rs
@@ -74,11 +74,12 @@ impl GearApi {
             .await?)
     }
 
-    pub async fn block_hash_to_number(&self, block: H256) -> AnyResult<u32> {
+    pub async fn block_hash_to_number(&self, block_hash: H256) -> AnyResult<u32> {
         self.api
-            .gear_block_number(Some(block))
+            .blocks()
+            .at(BlockRef::from_hash(block_hash))
             .await
-            .map_err(|err| anyhow!("Failed to get block number: {err}"))
+            .map(|block| block.number())
     }
 
     pub async fn block_number_to_hash(&self, block: u32) -> AnyResult<H256> {

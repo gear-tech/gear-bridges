a88f6d6d14e28b43263f1ed933d50c13dd6104af
Vadim Tokar <vtokar@gear.foundation>
2026-04-01 14:21:51 +0300

    fix new vara contract

--- js/frontend/src/features/history/hooks/use-pairs.ts
+++ js/frontend/src/features/history/hooks/use-pairs.ts
@@ -26,9 +26,14 @@ const PAIRS_QUERY = graphql(`
     }
   }
 `);
+const OLD_VARA_TOKEN = '0xdbf80fe5bd78b44510762770a14dc2a5b13a6bb167ff12c2edcc7ca3deadc16d';
+const NEW_VARA_TOKEN = '0x29c42c668012b1ce20720e4615229215023281ef4676fdc77bf047d7fbcb9d17';
 
-const derivePairs = ({ allPairs }: PairsQueryQuery) =>
-  allPairs?.nodes as (Pair & { varaToken: HexString; ethToken: HexString })[];
+const derivePairs = ({ allPairs }: PairsQueryQuery) => {
+  const pairs = allPairs?.nodes as (Pair & { varaToken: HexString; ethToken: HexString })[];
+
+  return pairs?.map((pair) => (pair.varaToken === OLD_VARA_TOKEN ? { ...pair, varaToken: NEW_VARA_TOKEN } : pair));
+};
 
 function usePairs() {
   const { NETWORK_PRESET } = useNetworkType();
